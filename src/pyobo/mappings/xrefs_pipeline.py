df7fe0eb7709da39b0e811f6d4eefa2f41419677
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-03-21 14:13:54 +0100

    Reorganize acquisition of external xrefs

--- src/pyobo/mappings/xrefs_pipeline.py
+++ src/pyobo/mappings/xrefs_pipeline.py
@@ -10,6 +10,7 @@ import click
 import pandas as pd
 
 from .extract_xrefs import get_all_xrefs
+from .xref_sources import get_cbms2019_xrefs_df, get_gilda_xrefs_df
 from ..getters import MissingOboBuild
 from ..path_utils import get_prefix_directory
 from ..registries import get_metaregistry
@@ -61,31 +62,11 @@ def _iterate_xref_dfs() -> Iterable[pd.DataFrame]:
         if not os.listdir(prefix_directory):
             os.rmdir(prefix_directory)
 
-    # Iterate some other ones
-    df = pd.read_csv(
-        GILDA_MAPPINGS, sep='\t', header=None, usecols=[0, 1, 3, 4],
-        names=['source_ns', 'source_id', 'target_ns', 'target_id'],
-    )
-    df['source'] = 'gilda'
-
-    # Fix chebi prefixs
-    for k in 'source_id', 'target_id':
-        df[k] = df[k].map(_fix_prefixes)
-
-    yield df[COLUMNS]
-
-
-def _fix_prefixes(s):
-    for prefix in ('CHEBI:', 'DOID;'):
-        if s.startswith(prefix):
-            return s[len(prefix):]
-    return s
+    yield get_gilda_xrefs_df()
+    yield get_cbms2019_xrefs_df()
 
 
 def _iterate_metaregistry():
     for prefix, _entry in sorted(get_metaregistry().items()):
         if prefix not in SKIP:
             yield prefix, _entry
-
-
-GILDA_MAPPINGS = 'https://raw.githubusercontent.com/indralab/gilda/master/gilda/resources/mesh_mappings.tsv'
