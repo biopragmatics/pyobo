1d5bde41634bfe30c576bb62dba4444e00823cd6
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-04-01 15:29:40 +0200

    Add HGNC encoding codes

--- src/pyobo/sources/hgnc.py
+++ src/pyobo/sources/hgnc.py
@@ -45,6 +45,43 @@ gene_xrefs = [
     ('snornabase', 'snornabase'),
 ]
 
+#: Encodings from https://www.genenames.org/cgi-bin/statistics
+#: To see all, do: ``cat hgnc_complete_set.json | jq .response.docs[].locus_type | sort | uniq``
+ENCODINGS = {
+    # protein-coding gene
+    'gene with protein product': 'GRP',
+    # non-coding RNA
+    'RNA, Y': 'GR',
+    'RNA, cluster': 'GR',
+    'RNA, long non-coding': 'GR',
+    'RNA, micro': 'GM',
+    'RNA, misc': 'GR',
+    'RNA, ribosomal': 'GR',
+    'RNA, small cytoplasmic': 'GR',
+    'RNA, small nuclear': 'GR',
+    'RNA, small nucleolar': 'GR',
+    'RNA, transfer': 'GR',
+    'RNA, vault': 'GR',
+    # phenotype
+    'phenotype only': 'G',
+    # pseudogene
+    'T cell receptor pseudogene': 'GRP',
+    'immunoglobulin pseudogene': 'GRP',
+    'immunoglobulin gene': 'GRP',
+    'pseudogene': 'G',
+    # other
+    'T cell receptor gene': 'GRP',
+    'complex locus constituent': 'G',
+    'endogenous retrovirus': 'G',
+    'fragile site': 'G',
+    'protocadherin': 'GRP',
+    'readthrough': 'G',
+    'region': 'G',
+    'transposable element': 'G',
+    'virus integration site': 'G',
+    'unknown': 'GRP',
+}
+
 
 def get_obo() -> Obo:
     """Get HGNC as OBO."""
