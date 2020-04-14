4a024fa76dc48c790195bf511735f3afb887d787
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-04-14 16:05:04 +0200

    Add first implementation of curie canonicalizer
    One test fails - when a CURIE is given with no priority and it's in a cluster of other things with no priority, it should be kept

--- src/pyobo/xrefdb/xrefs_pipeline.py
+++ src/pyobo/xrefdb/xrefs_pipeline.py
@@ -2,12 +2,17 @@
 
 """Pipeline for extracting all xrefs from OBO documents available."""
 
+import itertools as itt
 import os
 import urllib.error
-from typing import Iterable
+from dataclasses import dataclass, field
+from typing import Iterable, List, Mapping, Optional
 
 import click
+import networkx as nx
 import pandas as pd
+from more_itertools import pairwise
+from tqdm import tqdm
 
 from .sources import get_cbms2019_xrefs_df, get_gilda_xrefs_df
 from ..extract import get_xrefs_df
@@ -17,10 +22,120 @@ from ..registries import get_metaregistry
 
 SKIP = {
     'obi',
-    'ncbigene',  # too big, refs axquired from  other dbs
+    'ncbigene',  # too big, refs acquired from other dbs
     'pubchem.compound',  # to big, can't deal with this now
 }
 COLUMNS = ['source_ns', 'source_id', 'target_ns', 'target_id', 'source']
+
+
+@dataclass
+class Canonicalizer:
+    """Wraps a graph and priority list to allow getting the best identifier."""
+
+    #: A graph from :func:`get_graph_from_xref_df`
+    graph: nx.Graph
+    #: A list of prefixes. The ones with the lower index are higher priority
+    priority: List[str]
+
+    _priority: Mapping[str, int] = field(init=False)
+
+    def __post_init__(self):
+        """Initialize the priority map based on the priority list."""
+        self._priority = {
+            entry: len(self.priority) - i
+            for i, entry in enumerate(self.priority)
+        }
+
+    def _key(self, curie: str) -> int:
+        # Assume node is in graph
+        prefix = self.graph.nodes[curie]['prefix']
+        return self._priority.get(prefix, -1)
+
+    def canonicalize(self, curie: str) -> str:
+        """Get the best CURIE from the given CURIE."""
+        if curie not in self.graph:
+            return curie
+        #: CURIEs of possible targets
+        targets = nx.node_connected_component(self.graph, curie)
+        return max(targets, key=self._key)
+
+
+def get_graph_from_xref_df(df: pd.DataFrame) -> nx.Graph:
+    """Generate a graph from the mappings dataframe."""
+    rv = nx.Graph()
+
+    it = itt.chain(
+        df[['source_ns', 'source_id']].drop_duplicates().values,
+        df[['target_ns', 'target_id']].drop_duplicates().values,
+    )
+    it = tqdm(it, desc='loading curies', unit_scale=True)
+    for prefix, identifier in it:
+        rv.add_node(_to_curie(prefix, identifier), prefix=prefix, identifier=identifier)
+
+    it = tqdm(df.values, total=len(df.index), desc='loading xrefs', unit_scale=True)
+    for source_ns, source_id, target_ns, target_id, source in it:
+        rv.add_edge(
+            _to_curie(source_ns, source_id),
+            _to_curie(target_ns, target_id),
+            source=source,
+        )
+
+    return rv
+
+
+def _to_curie(prefix: str, identifier: str) -> str:
+    return f'{prefix}:{identifier}'
+
+
+def all_shortest_paths(graph: nx.Graph, source_curie: str, target_curie: str) -> List[List[Mapping[str, str]]]:
+    """Get all shortest paths between the two CURIEs."""
+    _paths = nx.all_shortest_paths(graph, source=source_curie, target=target_curie)
+    return [
+        [
+            dict(source=s, target=t, provenance=graph[s][t]['source'])
+            for s, t in pairwise(_path)
+        ]
+        for _path in _paths
+    ]
+
+
+def single_source_shortest_path(graph: nx.Graph, curie: str) -> Optional[Mapping[str, List[Mapping[str, str]]]]:
+    """Get the shortest path from the CURIE to all elements of its equivalence class.
+
+    Things that didn't work:
+
+    Unresponsive
+    ------------
+    .. code-block:: python
+
+        for curies in tqdm(nx.connected_components(graph), desc='filling connected components', unit_scale=True):
+            for c1, c2 in itt.combinations(curies, r=2):
+                if not graph.has_edge(c1, c2):
+                    graph.add_edge(c1, c2, inferred=True)
+
+    Way too slow
+    ------------
+    .. code-block:: python
+
+        for curie in tqdm(graph, total=graph.number_of_nodes(), desc='mapping connected components', unit_scale=True):
+            for incident_curie in nx.node_connected_component(graph, curie):
+                if not graph.has_edge(curie, incident_curie):
+                    graph.add_edge(curie, incident_curie, inferred=True)
+
+    Also consider the condensation of the graph:
+    https://networkx.github.io/documentation/stable/reference/algorithms/generated/networkx.algorithms.components.condensation.html#networkx.algorithms.components.condensation
+    """
+    if curie not in graph:
+        return None
+    rv = nx.single_source_shortest_path(graph, curie)
+    return {
+        k: [
+            dict(source=s, target=t, provenance=graph[s][t]['source'])
+            for s, t in pairwise(v)
+        ]
+        for k, v in rv.items()
+        if k != curie  # don't map to self
+    }
 
 
 def get_xref_df() -> pd.DataFrame:
