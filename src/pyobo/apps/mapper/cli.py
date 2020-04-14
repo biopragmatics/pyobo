dfe1324ef8d59ad878382247e111a613dea93bed
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-04-14 18:17:08 +0200

    Update mapper web app
    - add canonicalize endpoint
    - add default generation of xrefs file if none is present

--- src/pyobo/apps/mapper/cli.py
+++ src/pyobo/apps/mapper/cli.py
@@ -2,17 +2,18 @@
 
 """PyOBO's Mapping Service."""
 
+import os
 from functools import lru_cache
 from typing import Iterable, List, Mapping, Optional, Union
 
 import click
 import pandas as pd
 from flask import Blueprint, Flask, current_app, jsonify, url_for
-from werkzeug import LocalProxy
+from werkzeug.local import LocalProxy
 
+from pyobo.identifier_utils import normalize_curie
 from pyobo.xrefdb.xrefs_pipeline import (
-    all_shortest_paths, get_graph_from_xref_df, get_xref_df,
-    single_source_shortest_path,
+    Canonicalizer, all_shortest_paths, get_graph_from_xref_df, get_xref_df, single_source_shortest_path,
 )
 
 __all__ = [
@@ -21,6 +22,7 @@ __all__ = [
 ]
 
 graph = LocalProxy(lambda: current_app.config['graph'])
+canonicalizer: Canonicalizer = LocalProxy(lambda: current_app.config['canonicalizer'])
 
 
 @lru_cache()
@@ -41,13 +43,13 @@ search_blueprint = Blueprint('search', __name__)
 def home():
     """Show the home page."""
     example_url_1 = url_for(
-        single_source_mappings.__name__,
+        f'.{single_source_mappings.__name__}',
         curie='hgnc:6893',
     )
     example_url_2 = url_for(
-        all_mappings.__name__,
+        f'.{all_mappings.__name__}',
         source_curie='hgnc:6893',
-        target_curie='ensembl:ENSMUSP00000102605',
+        target_curie='ensembl:ENSG00000186868',
     )
     return f'''Use the /mappings endpoint. For example, <a href="{example_url_1}">{example_url_1}</a> or
         <a href="{example_url_2}">{example_url_2}</a>/
@@ -85,10 +87,49 @@ def all_mappings(source_curie: str, target_curie: str):
     return jsonify(_all_shortest_paths(source_curie, target_curie))
 
 
+@search_blueprint.route('/canonicalize/<curie>')
+def canonicalize_curie(curie: str):
+    """Return the best CURIE."""
+    # TODO maybe normalize the curie first?
+    norm_curie = normalize_curie(curie)
+    if not norm_curie:
+        return jsonify(
+            query=curie,
+            normalizable=False,
+        )
+
+    norm_curie = ':'.join(norm_curie)
+
+    rv = dict(query=curie)
+    if norm_curie != curie:
+        rv['norm_curie'] = norm_curie
+
+    if norm_curie not in graph:
+        rv['found'] = False
+    else:
+        result_curie = canonicalizer.canonicalize(norm_curie)
+        rv.update(
+            found=True,
+            result=result_curie,
+            mappings=url_for(
+                f'.{all_mappings.__name__}',
+                source_curie=norm_curie,
+                target_curie=result_curie,
+            ),
+        )
+
+    return jsonify(rv)
+
+
 def get_app(paths: Union[None, str, Iterable[str]] = None) -> Flask:
     """Build the Flask app."""
     if paths is None:
-        df = get_xref_df()
+        paths = os.path.join(os.path.expanduser('~'), 'Desktop', 'all_xrefs.tsv')
+        if os.path.exists(paths):
+            df = pd.read_csv(paths, sep='\t', dtype=str)
+        else:
+            df = get_xref_df()
+            df.to_csv(paths, sep='\t', index=False)
     elif isinstance(paths, str):
         df = pd.read_csv(paths, sep='\t', dtype=str)
     else:
@@ -102,12 +143,14 @@ def get_app(paths: Union[None, str, Iterable[str]] = None) -> Flask:
 def _get_app_from_xref_df(df: pd.DataFrame):
     app = Flask(__name__)
     app.config['graph'] = get_graph_from_xref_df(df)
+    # TODO allow for specification of priorities in the canonicalizer
+    app.config['canonicalizer'] = Canonicalizer(graph=app.config['graph'])
     app.register_blueprint(search_blueprint)
     return app
 
 
 @click.command()
-@click.option('-m', '--mappings-file', multiple=True, default=['/Users/cthoyt/Desktop/xrefs.tsv.gz'])
+@click.option('-x', '--mappings-file')
 @click.option('--port')
 @click.option('--host', type=int)
 def main(mappings_file, port: str, host: int):
