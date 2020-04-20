fc7ec34f423e93b64967bfea986bd70cae54a732
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-04-21 00:01:39 +0200

    Add more intact xrefs

--- src/pyobo/xrefdb/sources/__init__.py
+++ src/pyobo/xrefdb/sources/__init__.py
@@ -9,13 +9,14 @@ import pandas as pd
 from .cbms2019 import get_cbms2019_xrefs_df
 from .famplex import get_famplex_xrefs_df
 from .gilda import get_gilda_xrefs_df
-from .intact_complexportal import get_intact_complex_portal_xrefs_df
+from .intact import get_intact_complex_portal_xrefs_df, get_intact_reactome_xrefs_df
 
 __all__ = [
     'get_famplex_xrefs_df',
     'get_gilda_xrefs_df',
     'get_cbms2019_xrefs_df',
     'get_intact_complex_portal_xrefs_df',
+    'get_intact_reactome_xrefs_df',
     'iter_sourced_xref_dfs',
 ]
 
@@ -26,3 +27,4 @@ def iter_sourced_xref_dfs() -> Iterable[pd.DataFrame]:
     yield get_cbms2019_xrefs_df()
     yield get_famplex_xrefs_df()
     yield get_intact_complex_portal_xrefs_df()
+    yield get_intact_reactome_xrefs_df()
---
+++ src/pyobo/xrefdb/sources/intact.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+"""Get the xrefs from IntAct."""
+
+from typing import Mapping
+
+import pandas as pd
+
+from pyobo.cache_utils import cached_mapping
+from pyobo.path_utils import prefix_directory_join
+
+__all__ = [
+    'COMPLEXPORTAL_MAPPINGS',
+    'get_intact_complex_portal_xrefs_df',
+    'get_complexportal_mapping',
+    'get_intact_reactome_xrefs_df',
+    'get_reactome_mapping',
+]
+
+COMPLEXPORTAL_MAPPINGS = 'ftp://ftp.ebi.ac.uk/pub/databases/intact/current/various/cpx_ebi_ac_translation.txt'
+REACTOME_MAPPINGS = 'ftp://ftp.ebi.ac.uk/pub/databases/intact/current/various/reactome.dat'
+
+
+def _get_complexportal_df():
+    return pd.read_csv(COMPLEXPORTAL_MAPPINGS, sep='\t', header=None, names=['source_id', 'target_id'])
+
+
+def get_intact_complex_portal_xrefs_df() -> pd.DataFrame:
+    """Get IntAct-Complex Portal xrefs."""
+    df = _get_complexportal_df()
+    df['source_ns'] = 'intact'
+    df['target_ns'] = 'complexportal'
+    df['source'] = COMPLEXPORTAL_MAPPINGS
+    df = df[['source_ns', 'source_id', 'target_ns', 'target_id', 'source']]
+    return df
+
+
+@cached_mapping(
+    path=prefix_directory_join('intact', 'cache', 'xrefs', 'complexportal.tsv'),
+    header=[f'intact_id', f'complexportal_id'],
+)
+def get_complexportal_mapping() -> Mapping[str, str]:
+    """Get IntAct to Complex Portal mapping.
+
+    Is basically equivalent to:
+
+    .. code-block:: python
+
+        from pyobo import get_filtered_xrefs
+        intact_complexportal_mapping = get_filtered_xrefs('intact', 'complexportal')
+    """
+    df = _get_complexportal_df()
+    return dict(df.values)
+
+
+def _get_reactome_df():
+    return pd.read_csv(REACTOME_MAPPINGS, sep='\t', header=None, names=['source_id', 'target_id'])
+
+
+def get_intact_reactome_xrefs_df() -> pd.DataFrame:
+    """Get IntAct-Reactome xrefs."""
+    df = _get_reactome_df()
+    df['source_ns'] = 'intact'
+    df['target_ns'] = 'reactome'
+    df['source'] = REACTOME_MAPPINGS
+    df = df[['source_ns', 'source_id', 'target_ns', 'target_id', 'source']]
+    return df
+
+
+@cached_mapping(
+    path=prefix_directory_join('intact', 'cache', 'xrefs', 'reactome.tsv'),
+    header=[f'intact_id', f'reactome_id'],
+)
+def get_reactome_mapping() -> Mapping[str, str]:
+    """Get IntAct to Reactome mapping.
+
+    Is basically equivalent to:
+
+    .. code-block:: python
+
+        from pyobo import get_filtered_xrefs
+        intact_complexportal_mapping = get_filtered_xrefs('intact', 'reactome')
+    """
+    df = _get_complexportal_df()
+    return dict(df.values)
