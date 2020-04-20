5cbbbe31f79446c18267c1181518c4e0a100c01f
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-04-20 02:16:44 +0200

    Add home to resolver

--- src/pyobo/apps/resolver.py
+++ src/pyobo/apps/resolver.py
@@ -3,12 +3,38 @@
 """PyOBO's Resolution Service."""
 
 import click
-from flask import Blueprint, Flask, jsonify
+from flask import Blueprint, Flask, jsonify, url_for
 
 from pyobo.extract import get_name
 from pyobo.identifier_utils import normalize_curie
 
 resolve_blueprint = Blueprint('resolver', __name__)
+
+
+@resolve_blueprint.route('/')
+def home():
+    """Serve the home page."""
+    success = url_for(
+        f'.{resolve.__name__}',
+        curie='DOID:14330',
+    )
+    remapped = url_for(
+        f'.{resolve.__name__}',
+        curie='do:14330',
+    )
+    failure = url_for(
+        f'.{resolve.__name__}',
+        curie='DOID:00000',
+    )
+
+    return f'''
+    <h1>PyOBO Resolver</h1>
+    <ul>
+    <li>Example successful lookup: <a href="{success}">{success}</a></li>
+    <li>Example successful lookup by remapping prefix: <a href="{remapped}">{remapped}</a></li>
+    <li>Example unsuccessful lookup: <a href="{failure}">{failure}</a></li>
+    </ui>
+    '''
 
 
 @resolve_blueprint.route('/resolve/<curie>')
