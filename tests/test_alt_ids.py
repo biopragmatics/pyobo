617c5613c330109e77e157b03525fc112fcc899e
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-05-28 15:50:23 +0200

    Add tests for alt ids

---
+++ tests/test_alt_ids.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+"""Tests for alternative identifiers."""
+
+import unittest
+
+from pyobo import get_primary_identifier
+
+
+class TestAltIds(unittest.TestCase):
+    """Tests for alternative identifiers."""
+
+    def test_get_primary(self):
+        """Test upgrading an obsolete identifier."""
+        primary_id = get_primary_identifier('go', '0001071')
+        self.assertIsNotNone(primary_id)
+        self.assertEqual('0003700', primary_id)
+
+    def test_already_primary(self):
+        """Test when you give a primary id."""
+        primary_id = get_primary_identifier('go', '0003700')
+        self.assertIsNotNone(primary_id)
+        self.assertEqual('0003700', primary_id)
