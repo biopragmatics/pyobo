f4e47ea89c829b60b16c20ec6607b2c2ce57f85a
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-05-30 23:45:46 +0200

    Make generation of ooh-na-na easier

--- src/pyobo/xrefdb/cli.py
+++ src/pyobo/xrefdb/cli.py
@@ -11,12 +11,13 @@ import pandas as pd
 
 from .xrefs_pipeline import Canonicalizer, _iter_ooh_na_na, get_xref_df, summarize_xref_df
 from ..cli_utils import verbose_option
+from ..constants import PYOBO_HOME
 from ..identifier_utils import UNHANDLED_NAMESPACES
 
 directory_option = click.option(
     '-d', '--directory',
     type=click.Path(dir_okay=True, file_okay=False, exists=True),
-    default=os.getcwd(),
+    default=PYOBO_HOME,
 )
 
 
@@ -44,7 +45,7 @@ def javerts_xrefs(directory: str):  # noqa: D202
 
     # Export a summary dataframe
     summary_df = summarize_xref_df(xrefs_df)
-    _write_tsv(summary_df, 'inspector_javerts_xref_summary.tsv')
+    _write_tsv(summary_df, 'inspector_javerts_xrefs_summary.tsv')
 
     # Export the namespaces that haven't been handled yet
     unmapped_path = os.path.join(directory, 'inspector_javerts_unmapped_xrefs.tsv')
@@ -61,15 +62,17 @@ def ooh_na_na(directory: str):
     """Make the prefix-identifier-name dump."""
     c = Counter()
 
-    path = os.path.join(directory, 'ooh_na_na.tsv.gz')
-    with gzip.open(path, mode='wt') as gzipped_file:
+    db_path = os.path.join(directory, 'ooh_na_na.tsv.gz')
+    click.echo(f'Writing Ooh-Na-Na to {db_path}')
+    with gzip.open(db_path, mode='wt') as gzipped_file:
         print('prefix', 'identifier', 'name', sep='\t', file=gzipped_file)
         for prefix, identifier, name in _iter_ooh_na_na():
             c[prefix] += 1
             print(prefix, identifier, name, sep='\t', file=gzipped_file)
 
-    path = os.path.join(directory, 'summary.tsv')
-    with open(path, 'w') as file:
+    summary_path = os.path.join(directory, 'ooh_na_na_summary.tsv')
+    click.echo(f'Writing Ooh-Na-Na summary to {summary_path}')
+    with open(summary_path, 'w') as file:
         for k, v in c.most_common():
             print(k, v, sep='\t', file=file)
 
