9d529fb022c35f362b206f0e6806ad8610319e86
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-08-11 14:55:58 +0200

    Add chembl xrefs

--- src/pyobo/xrefdb/sources/__init__.py
+++ src/pyobo/xrefdb/sources/__init__.py
@@ -7,6 +7,7 @@ from typing import Iterable
 import pandas as pd
 
 from .cbms2019 import get_cbms2019_xrefs_df
+from .chembl import get_chembl_compound_equivalences, get_chembl_protein_equivalences
 from .compath import iter_compath_dfs
 from .famplex import get_famplex_xrefs_df
 from .gilda import get_gilda_xrefs_df
@@ -22,6 +23,8 @@ __all__ = [
     'get_intact_complex_portal_xrefs_df',
     'get_intact_reactome_xrefs_df',
     'iter_sourced_xref_dfs',
+    'get_chembl_compound_equivalences',
+    'get_chembl_protein_equivalences',
 ]
 
 
@@ -36,3 +39,5 @@ def iter_sourced_xref_dfs() -> Iterable[pd.DataFrame]:
     yield from iter_compath_dfs()
     yield from iterate_wikidata_dfs()
     yield get_pubchem_mesh_df()
+    yield get_chembl_compound_equivalences()
+    yield get_chembl_protein_equivalences()
