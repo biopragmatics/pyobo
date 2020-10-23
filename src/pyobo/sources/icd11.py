3e62b48c99d90d445df3b029beb39a26a05217e3
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-10-23 13:17:14 +0200

    Abstract code for all ICD and implement icd10

--- src/pyobo/sources/icd11.py
+++ src/pyobo/sources/icd11.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 
-"""Converter for ICD."""
+"""Convert ICD11 to OBO.
 
-import datetime
+Run with python -m pyobo.sources.icd11 -v
+"""
+
 import json
 import logging
 import os
-from typing import Any, Iterable, List, Mapping, Set
+from typing import Any, Iterable, Mapping
 
 import click
-import requests
-from cachier import cachier
 from tqdm import tqdm
 
 from pyobo import Synonym
 from pyobo.cli_utils import verbose_option
 from pyobo.path_utils import get_prefix_directory
-from pyobo.sources.icd_utils import CLIENT_ID, CLIENT_SECRET, ICD11_TOP_LEVEL_URL, TOKEN_URL
+from pyobo.sources.icd_utils import ICD11_TOP_LEVEL_URL, get_child_identifiers, get_icd, visiter
 from pyobo.struct import Obo, Reference, Term
 
 logger = logging.getLogger(__name__)
@@ -28,27 +28,10 @@ def get_obo() -> Obo:
     """Get ICD11 as OBO."""
     return Obo(
         ontology=PREFIX,
-        name='ICD-11',
+        name='International Statistical Classification of Diseases and Related Health Problems 11th Revision',
         iter_terms=iterate_icd11,
         auto_generated_by=f'bio2obo:{PREFIX}',
     )
-
-
-@cachier(stale_after=datetime.timedelta(hours=1))
-def get_icd_api_headers() -> Mapping[str, str]:
-    """Get the headers, and refresh every hour."""
-    grant_type = 'client_credentials'
-    body_params = {'grant_type': grant_type}
-    tqdm.write('getting ICD API token')
-    res = requests.post(TOKEN_URL, data=body_params, auth=(CLIENT_ID, CLIENT_SECRET))
-    res_json = res.json()
-    access_type = res_json['token_type']
-    access_token = res_json['access_token']
-    return {
-        'API-Version': 'v2',
-        'Accept-Language': 'en',
-        'Authorization': f'{access_type} {access_token}',
-    }
 
 
 def iterate_icd11() -> Iterable[Term]:
@@ -59,7 +42,7 @@ def iterate_icd11() -> Iterable[Term]:
     Get ready to be patient - the API token expires every hour so there's
     a caching mechanism with :mod:`cachier` that gets a new one every hour.
     """
-    res = requests.get(ICD11_TOP_LEVEL_URL, headers=get_icd_api_headers())
+    res = get_icd(ICD11_TOP_LEVEL_URL)
     res_json = res.json()
 
     version = res_json['releaseId']
@@ -71,48 +54,17 @@ def iterate_icd11() -> Iterable[Term]:
     tqdm.write(f'There are {len(res_json["child"])} top level entities')
 
     visited_identifiers = set()
-    for identifier in _get_child_identifiers(res_json):
-        yield from _visit(identifier, visited_identifiers, directory)
+    for identifier in get_child_identifiers(ICD11_TOP_LEVEL_URL, res_json):
+        yield from visiter(
+            identifier,
+            visited_identifiers,
+            directory,
+            endpoint=ICD11_TOP_LEVEL_URL,
+            converter=_extract_icd11,
+        )
 
 
-def _get_entity(identifier: str):
-    url = f'{ICD11_TOP_LEVEL_URL}/{identifier}'
-    #  tqdm.write(f'query {identifier} at {url}')
-    res = requests.get(url, headers=get_icd_api_headers())
-    return res.json()
-
-
-def _get_child_identifiers(res_json: Mapping[str, Any]) -> List[str]:
-    return [
-        url[len(ICD11_TOP_LEVEL_URL):].lstrip('/')
-        for url in res_json.get('child', [])
-    ]
-
-
-def _visit(
-    identifier: str,
-    visited_identifiers: Set[str],
-    directory: str,
-) -> Iterable[Term]:
-    path = os.path.join(directory, f'{identifier}.json')
-    if identifier in visited_identifiers:
-        return
-    visited_identifiers.add(identifier)
-
-    if os.path.exists(path):
-        with open(path) as file:
-            res_json = json.load(file)
-    else:
-        res_json = _get_entity(identifier)
-        with open(path, 'w') as file:
-            json.dump(res_json, file, indent=2)
-
-    yield _extract(res_json)
-    for identifier in _get_child_identifiers(res_json):
-        yield from _visit(identifier, visited_identifiers, directory)
-
-
-def _extract(res_json) -> Term:
+def _extract_icd11(res_json: Mapping[str, Any]) -> Term:
     identifier = res_json['@id'][len(ICD11_TOP_LEVEL_URL):].lstrip('/')
     definition = res_json['definition']['@value'] if 'definition' in res_json else None
     name = res_json['title']['@value']
@@ -135,12 +87,9 @@ def _extract(res_json) -> Term:
 
 @click.command()
 @verbose_option
-def main():
-    """Download the ICD11 data."""
-    # get_obo().write_default(use_tqdm=True)
-    for _ in tqdm(iterate_icd11(), desc=f'Downloading {PREFIX}', unit_scale=True):
-        pass
+def _main():
+    get_obo().write_default(use_tqdm=True)
 
 
 if __name__ == '__main__':
-    main()
+    _main()
--- src/pyobo/sources/icd_utils.py
+++ src/pyobo/sources/icd_utils.py
@@ -2,8 +2,91 @@
 
 """Utilities or interacting with the ICD API."""
 
+import datetime
+import json
+import os
+from typing import Any, Callable, Iterable, List, Mapping, Set
+
+import requests
+from cachier import cachier
+from tqdm import tqdm
+
+from pyobo import Term
+
 CLIENT_ID = '7570c9bb-728c-46b9-b729-ef4a3675790e_e55b6899-2d00-4804-adf2-a3195d210316'
 CLIENT_SECRET = 'FnncINDvJ1zoDrLsszBsjXAeJ8bQEc7TBAOo5hBu2G0='
 
 TOKEN_URL = 'https://icdaccessmanagement.who.int/connect/token'
-ICD11_TOP_LEVEL_URL = 'https://id.who.int/icd/entity'
+
+ICD_BASE_URL = 'https://id.who.int/icd'
+
+ICD11_TOP_LEVEL_URL = f'{ICD_BASE_URL}/entity'
+ICD10_TOP_LEVEL_URL = f'{ICD_BASE_URL}/release/10/2016'
+
+
+def get_icd(url: str) -> requests.Response:
+    """Get an ICD API endpoint."""
+    return requests.get(url, headers=get_icd_api_headers())
+
+
+def _get_entity(endpoint: str, identifier: str):
+    url = f'{endpoint}/{identifier}'
+    # tqdm.write(f'query {identifier} at {url}')
+    res = get_icd(url)
+    return res.json()
+
+
+def get_child_identifiers(endpoint: str, res_json: Mapping[str, Any]) -> List[str]:
+    """Ge the child identifiers."""
+    return [
+        url[len(endpoint):].lstrip('/')
+        for url in res_json.get('child', [])
+    ]
+
+
+@cachier(stale_after=datetime.timedelta(minutes=45))
+def get_icd_api_headers() -> Mapping[str, str]:
+    """Get the headers, and refresh every hour."""
+    grant_type = 'client_credentials'
+    body_params = {'grant_type': grant_type}
+    tqdm.write('getting ICD API token')
+    res = requests.post(TOKEN_URL, data=body_params, auth=(CLIENT_ID, CLIENT_SECRET))
+    res_json = res.json()
+    access_type = res_json['token_type']
+    access_token = res_json['access_token']
+    return {
+        'API-Version': 'v2',
+        'Accept-Language': 'en',
+        'Authorization': f'{access_type} {access_token}',
+    }
+
+
+def visiter(
+    identifier: str,
+    visited_identifiers: Set[str],
+    directory: str,
+    *,
+    endpoint: str,
+    converter: Callable[[Mapping[str, Any]], Term],
+) -> Iterable[Term]:
+    """Iterate over all terms from the ICD endpoint."""
+    path = os.path.join(directory, f'{identifier}.json')
+    if identifier in visited_identifiers:
+        return
+    visited_identifiers.add(identifier)
+
+    if os.path.exists(path):
+        with open(path) as file:
+            res_json = json.load(file)
+    else:
+        res_json = _get_entity(endpoint, identifier)
+        with open(path, 'w') as file:
+            json.dump(res_json, file, indent=2)
+
+    yield converter(res_json)
+    for identifier in get_child_identifiers(endpoint, res_json):
+        yield from visiter(
+            identifier, visited_identifiers, directory,
+            converter=converter,
+            endpoint=endpoint,
+        )
