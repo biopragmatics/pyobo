b36c3433abe6a0c5c879da946a160e3e9d91733d
Charles Tapley Hoyt <cthoyt@gmail.com>
2020-12-27 23:53:17 +0100

    Update compath.py

--- src/pyobo/xrefdb/sources/compath.py
+++ src/pyobo/xrefdb/sources/compath.py
@@ -5,20 +5,20 @@
 from typing import Iterable
 
 import pandas as pd
+from pystow.utils import get_commit
 
-from ...constants import PROVENANCE, SOURCE_ID, SOURCE_PREFIX, TARGET_ID, TARGET_PREFIX, XREF_COLUMNS
+from pyobo.constants import PROVENANCE, SOURCE_ID, SOURCE_PREFIX, TARGET_ID, TARGET_PREFIX, XREF_COLUMNS
 
 __all__ = [
     'iter_compath_dfs',
 ]
 
-BASE_URL = 'https://raw.githubusercontent.com/ComPath/compath-resources/master/mappings'
 
-
-def _get_df(name) -> pd.DataFrame:
-    url = f'{BASE_URL}/{name}'
+def _get_df(name: str, *, sha: str, sep: str = ',') -> pd.DataFrame:
+    url = f'https://raw.githubusercontent.com/ComPath/compath-resources/{sha}/mappings/{name}'
     df = pd.read_csv(
-        url, sep=',',
+        url,
+        sep=sep,
         usecols=['Source Resource', 'Source ID', 'Mapping Type', 'Target Resource', 'Target ID'],
     )
     df.rename(
@@ -39,15 +39,21 @@ def _get_df(name) -> pd.DataFrame:
 
 def iter_compath_dfs() -> Iterable[pd.DataFrame]:
     """Iterate over all ComPath mappings."""
-    yield _get_df('kegg_reactome.csv')
-    yield _get_df('kegg_wikipathways.csv')
-    yield _get_df('pathbank_kegg.csv')
-    yield _get_df('pathbank_reactome.csv')
-    yield _get_df('pathbank_wikipathways.csv')
-    yield _get_df('special_mappings.csv')
-    yield _get_df('wikipathways_reactome.csv')
+    sha = get_commit('ComPath', 'compath-resources')
+
+    yield _get_df('kegg_reactome.csv', sha=sha)
+    yield _get_df('kegg_wikipathways.csv', sha=sha)
+    yield _get_df('pathbank_kegg.csv', sha=sha)
+    yield _get_df('pathbank_reactome.csv', sha=sha)
+    yield _get_df('pathbank_wikipathways.csv', sha=sha)
+    yield _get_df('special_mappings.csv', sha=sha)
+    yield _get_df('wikipathways_reactome.csv', sha=sha)
 
 
-def get_compath_xrefs_df() -> Iterable[pd.DataFrame]:
+def get_compath_xrefs_df() -> pd.DataFrame:
     """Iterate over all ComPath mappings."""
     return pd.concat(iter_compath_dfs())
+
+
+if __name__ == '__main__':
+    print(get_compath_xrefs_df().head())
