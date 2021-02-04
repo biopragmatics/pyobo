49050d652403c644031fac627da9ecdb57218b3f
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-02-04 11:42:31 +0100

    Enable choose directory

--- src/pyobo/cli_database.py
+++ src/pyobo/cli_database.py
@@ -5,6 +5,9 @@
 Run with ``obo database <subcommand>``.
 """
 
+import os
+from typing import Optional
+
 import click
 import pandas as pd
 
@@ -19,67 +22,80 @@ def database():
     """Build the PyOBO Database."""
 
 
+directory_option = click.option(
+    '--directory',
+    type=click.Path(dir_okay=True, file_okay=False, exists=True),
+)
+
+
 @database.command()
 @verbose_option
+@directory_option
 @click.pass_context
-def build(ctx: click.Context):
+def build(ctx: click.Context, directory: str):
     """Build all databases."""
     click.secho('Alternate Identifiers', fg='cyan', bold=True)
-    ctx.invoke(alts)
+    ctx.invoke(alts, directory=directory)
     click.secho('Synonyms', fg='cyan', bold=True)
-    ctx.invoke(synonyms)
+    ctx.invoke(synonyms, directory=directory)
     click.secho('Xrefs', fg='cyan', bold=True)
-    ctx.invoke(xrefs)
+    ctx.invoke(xrefs, directory=directory)
     click.secho('Names', fg='cyan', bold=True)
-    ctx.invoke(names)
+    ctx.invoke(names, directory=directory)
+    # TODO relations
 
 
 @database.command()
 @verbose_option
+@directory_option
 @click.option('--no-strict', is_flag=True)
-def names(no_strict: bool):
+def names(directory: str, no_strict: bool):
     """Make the prefix-identifier-name dump."""
     db_output_helper(
         _iter_ooh_na_na,
         'names',
         ('prefix', 'identifier', 'name'),
         strict=not no_strict,
+        directory=directory,
     )
 
 
 @database.command()
 @verbose_option
-def alts():
+@directory_option
+def alts(directory: str):
     """Make the prefix-alt-id dump."""
-    db_output_helper(_iter_alts, 'alts', ('prefix', 'identifier', 'alt'))
+    db_output_helper(_iter_alts, 'alts', ('prefix', 'identifier', 'alt'), directory=directory)
 
 
 @database.command()
 @verbose_option
-def synonyms():
+@directory_option
+def synonyms(directory: str):
     """Make the prefix-identifier-synonym dump."""
-    db_output_helper(_iter_synonyms, 'synonyms', ('prefix', 'identifier', 'synonym'))
+    db_output_helper(_iter_synonyms, 'synonyms', ('prefix', 'identifier', 'synonym'), directory=directory)
 
 
 @database.command()
 @verbose_option
-def xrefs():  # noqa: D202
+@directory_option
+def xrefs(directory: str):  # noqa: D202
     """Make the prefix-identifier-xref dump."""
     xrefs_df = get_xref_df(rebuild=True, force=False)
 
     # Export all xrefs
-    _write_tsv(xrefs_df, 'xrefs.tsv.gz')
+    _write_tsv(xrefs_df, 'xrefs.tsv.gz', directory=directory)
 
     # Export a sample of xrefs
-    _write_tsv(xrefs_df.head(), 'xrefs_sample.tsv')
+    _write_tsv(xrefs_df.head(), 'xrefs_sample.tsv', directory=directory)
 
     # Export a summary dataframe
     summary_df = summarize_xref_df(xrefs_df)
-    _write_tsv(summary_df, 'xrefs_summary.tsv')
+    _write_tsv(summary_df, 'xrefs_summary.tsv', directory=directory)
 
 
-def _write_tsv(df: pd.DataFrame, name: str) -> None:
-    df.to_csv(DATABASE_DIRECTORY / name, sep='\t', index=False)
+def _write_tsv(df: pd.DataFrame, name: str, *, directory: Optional[str] = None) -> None:
+    df.to_csv(os.path.join(directory or DATABASE_DIRECTORY, name), sep='\t', index=False)
 
 
 if __name__ == '__main__':
