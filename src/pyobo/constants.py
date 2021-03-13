eb18f227b180f20bd895ca86b505d258cdb302f5
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-03-13 20:13:46 +0100

    Cleanup constants

--- src/pyobo/constants.py
+++ src/pyobo/constants.py
@@ -10,7 +10,6 @@ import bioversions
 import pystow
 
 __all__ = [
-    'PYOBO_HOME',
     'RAW_DIRECTORY',
     'DATABASE_DIRECTORY',
     'SPECIES_REMAPPING',
@@ -20,11 +19,11 @@ __all__ = [
 
 logger = logging.getLogger(__name__)
 
-PYOBO_MODULE = pystow.Module.from_key('pyobo')
-PYOBO_HOME = PYOBO_MODULE.base
+PYOBO_MODULE = pystow.module('pyobo')
 RAW_MODULE = PYOBO_MODULE.submodule('raw')
 RAW_DIRECTORY = RAW_MODULE.base
-DATABASE_DIRECTORY = PYOBO_MODULE.get('database')
+DATABASE_MODULE = PYOBO_MODULE.submodule('database')
+DATABASE_DIRECTORY = DATABASE_MODULE.base
 
 SPECIES_REMAPPING = {
     "Canis familiaris": "Canis lupus familiaris",
@@ -62,8 +61,8 @@ ALTS_FILE = 'alts.tsv.gz'
 
 def get_sqlalchemy_uri() -> str:
     """Get the SQLAlchemy URI."""
-    default_db_path = (PYOBO_HOME / 'pyobo.db').as_posix()
-    default_value = f'sqlite:///{default_db_path}'
+    default_db_path = PYOBO_MODULE.join('pyobo.db')
+    default_value = f'sqlite:///{default_db_path.as_posix()}'
     return pystow.get_config('pyobo', 'sqlalchemy_uri', fallback=default_value)
 
 
