6021beaf793ecf3be28fcd4d8bc6f0b90da54639
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-03-13 22:25:31 +0100

    Rewrite MSigDB importer

--- src/pyobo/sources/msigdb.py
+++ src/pyobo/sources/msigdb.py
@@ -4,31 +4,20 @@
 
 import logging
 from typing import Iterable
+from xml.etree import ElementTree
 
 import bioversions
 import click
 from more_click import verbose_option
+from tqdm import tqdm
 
-from .gmt_utils import parse_gmt_file
-from ..struct import Obo, Reference, Term
-from ..struct.typedef import has_part
+from ..struct import Obo, Reference, Term, has_part
 from ..utils.path import ensure_path
 
 logger = logging.getLogger(__name__)
 
 PREFIX = 'msigdb'
 BASE_URL = 'https://data.broadinstitute.org/gsea-msigdb/msigdb/release'
-
-
-def ensure_msigdb_path(version: str):
-    """Download the GSEA data and return the path."""
-    xml_url = f'{BASE_URL}/{version}/msigdb.v{version}.xml'
-    entrez_url = f'{BASE_URL}/{version}/msigdb.v{version}.entrez.gmt'
-    hgnc_url = f'{BASE_URL}/{version}/msigdb.v{version}.symbols.gmt'
-    xml_path = ensure_path(prefix=PREFIX, url=xml_url, version=version)
-    entrez_path = ensure_path(prefix=PREFIX, url=entrez_url, version=version)
-    hgnc_path = ensure_path(prefix=PREFIX, url=hgnc_url, version=version)
-    return xml_path, entrez_path, hgnc_path
 
 
 def get_obo() -> Obo:
@@ -45,22 +34,84 @@ def get_obo() -> Obo:
     )
 
 
+_SPECIES = {
+    'Homo sapiens': '9606',
+    'Mus musculus': '10090',
+    'Rattus norvegicus': '10116',
+    'Macaca mulatta': '9544',
+    'Danio rerio': '7955',
+}
+
+REACTOME_URL_PREFIX = 'https://www.reactome.org/content/detail/'
+GO_URL_PREFIX = 'http://amigo.geneontology.org/amigo/term/GO:'
+
+
 def iter_terms(version: str) -> Iterable[Term]:
     """Get MSigDb terms."""
-    _, path, _ = ensure_msigdb_path(version)
-    for identifier, name, genes in parse_gmt_file(path):
+    xml_url = f'{BASE_URL}/{version}/msigdb_v{version}.xml'
+    path = ensure_path(prefix=PREFIX, url=xml_url, version=version)
+    tree = ElementTree.parse(path)
+
+    for entry in tqdm(tree.getroot(), desc=f'{PREFIX} v{version}'):
+        attrib = dict(entry.attrib)
+        tax_id = _SPECIES[attrib['ORGANISM']]
+
+        reference_id = attrib['PMID'].strip()
+        if not reference_id:
+            reference = None
+        elif reference_id.startswith('GSE'):
+            reference = Reference('gse', reference_id)
+        else:
+            reference = Reference('pubmed', reference_id)
+
+        # NONE have the entry "HISTORICAL_NAME"
+        # historical_name = thing.attrib['HISTORICAL_NAME']
+
+        identifier = attrib['SYSTEMATIC_NAME']
+        name = attrib['STANDARD_NAME']
+        is_obsolete = attrib['CATEGORY_CODE'] == 'ARCHIVED'
+
         term = Term(
-            reference=Reference(prefix=PREFIX, identifier=identifier, name=name),
+            reference=Reference(PREFIX, identifier, name),
+            definition=attrib["DESCRIPTION_FULL"].strip() or attrib["DESCRIPTION_BRIEF"].strip() or None,
+            provenance=reference and [reference],
+            is_obsolete=is_obsolete,
         )
-        for ncbigene_id in genes:
-            term.append_relationship(has_part, Reference(prefix='ncbigene', identifier=ncbigene_id))
+        for key in ['CATEGORY_CODE', 'SUB_CATEGORY_CODE', 'CONTRIBUTOR', 'EXACT_SOURCE', 'EXTERNAL_DETAILS_URL']:
+            value = attrib[key].strip()
+            if value:
+                term.append_property(key.lower(), value)
+
+        term.set_species(tax_id)
+
+        contributor = attrib['CONTRIBUTOR']
+        external_id = attrib['EXACT_SOURCE']
+        external_details = attrib['EXTERNAL_DETAILS_URL']
+        if contributor == 'WikiPathways':
+            if not external_id:
+                logger.warning('missing %s source: msigdb:%s (%s)', contributor, identifier, external_details)
+            term.append_xref(Reference('wikipathways', external_id))
+        elif contributor == 'Reactome':
+            if not external_id:
+                logger.warning('missing %s source: msigdb:%s (%s)', contributor, identifier, external_details)
+            term.append_xref(Reference('reactome', external_id))
+        elif contributor == 'Gene Ontology':
+            if not external_id:
+                external_id = external_details[len(GO_URL_PREFIX):]
+            if not external_id:
+                logger.warning('missing %s source: msigdb:%s (%s)', contributor, identifier, external_details)
+            term.append_xref(Reference('go', external_id))
+
+        for ncbigene_id in attrib['MEMBERS_EZID'].strip().split(','):
+            if ncbigene_id:
+                term.append_relationship(has_part, Reference(prefix='ncbigene', identifier=ncbigene_id))
         yield term
 
 
 @click.command()
 @verbose_option
 def _main():
-    get_obo().write_default()
+    get_obo().write_default(force=True)
 
 
 if __name__ == '__main__':
