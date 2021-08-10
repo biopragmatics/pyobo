0b09006a2dfc016d1cc06fcbd57a59c4f4795cd0
Benjamin M. Gyori <ben.gyori@gmail.com>
2021-08-10 08:44:32 -0400

    Fix interpro processing (#116)
    * Fix interpro processing
    
    * Fix parents
    
    * Fix versioning
    
    Co-authored-by: Charles Tapley Hoyt <cthoyt@gmail.com>

--- src/pyobo/sources/interpro.py
+++ src/pyobo/sources/interpro.py
@@ -27,7 +27,7 @@ INTERPRO_PROTEIN_COLUMNS = [
 ]
 
 
-def get_obo() -> Obo:
+def get_obo(force: bool = False) -> Obo:
     """Get InterPro as OBO."""
     version = bioversions.get_version(PREFIX)
 
@@ -37,21 +37,25 @@ def get_obo() -> Obo:
         data_version=version,
         auto_generated_by=f"bio2obo:{PREFIX}",
         iter_terms=iter_terms,
-        iter_terms_kwargs=dict(version=version),
+        iter_terms_kwargs=dict(version=version, force=force),
     )
 
 
-def iter_terms(*, version: str, proteins: bool = False) -> Iterable[Term]:
+def iter_terms(*, version: str, proteins: bool = False, force: bool = False) -> Iterable[Term]:
     """Get InterPro terms."""
-    parents = get_interpro_tree(version=version)
-    interpro_to_gos = get_interpro_go_df(version=version)
-    interpro_to_proteins = get_interpro_to_proteins_df(version=version) if proteins else {}
+    parents = get_interpro_tree(version=version, force=force)
+    interpro_to_gos = get_interpro_go_df(version=version, force=force)
+    interpro_to_proteins = (
+        get_interpro_to_proteins_df(version=version, force=force) if proteins else {}
+    )
 
     entries_df = ensure_df(
         PREFIX,
         url=f"ftp://ftp.ebi.ac.uk/pub/databases/interpro/{version}/entry.list",
+        name="entries.tsv",
         skiprows=1,
         names=("ENTRY_AC", "ENTRY_TYPE", "ENTRY_NAME"),
+        version=version,
     )
 
     references = {
@@ -61,6 +65,7 @@ def iter_terms(*, version: str, proteins: bool = False) -> Iterable[Term]:
 
     for identifier, entry_type, _ in tqdm(entries_df.values):
         xrefs = []
+        # TODO there should be a relation here and not an xref
         for go_id, go_name in interpro_to_gos.get(identifier, []):
             xrefs.append(Reference("go", go_id, go_name))
 
@@ -75,29 +80,29 @@ def iter_terms(*, version: str, proteins: bool = False) -> Iterable[Term]:
         yield term
 
 
-def get_interpro_go_df(version: str) -> Mapping[str, Set[Tuple[str, str]]]:
+def get_interpro_go_df(version: str, force: bool = False) -> Mapping[str, Set[Tuple[str, str]]]:
     """Get InterPro to Gene Ontology molecular function mapping."""
     url = f"ftp://ftp.ebi.ac.uk/pub/databases/interpro/{version}/interpro2go"
-    path = ensure_path(PREFIX, url=url, name="interpro2go.tsv", version=version)
+    path = ensure_path(PREFIX, url=url, name="interpro2go.tsv", version=version, force=force)
     return get_go_mapping(path, prefix=PREFIX)
 
 
-def get_interpro_tree(version: str):
+def get_interpro_tree(version: str, force: bool = False):
     """Get InterPro Data source."""
     url = f"ftp://ftp.ebi.ac.uk/pub/databases/interpro/{version}/ParentChildTreeFile.txt"
-    path = ensure_path(PREFIX, url=url, version=version)
+    path = ensure_path(PREFIX, url=url, version=version, force=force)
     with open(path) as f:
         return _parse_tree_helper(f)
 
 
 def _parse_tree_helper(lines: Iterable[str]):
-    rv = defaultdict(list)
+    rv1 = defaultdict(list)
     previous_depth, previous_id = 0, None
     stack = [previous_id]
 
     for line in tqdm(lines, desc="parsing InterPro tree"):
         depth = _count_front(line)
-        parent_id, _ = line[depth:].split("::")
+        parent_id, _ = line[depth:].split("::", maxsplit=1)
 
         if depth == 0:
             stack.clear()
@@ -110,11 +115,15 @@ def _parse_tree_helper(lines: Iterable[str]):
                 del stack[-1]
 
             child_id = stack[-1]
-            rv[child_id].append(parent_id)
+            rv1[child_id].append(parent_id)
 
         previous_depth, previous_id = depth, parent_id
 
-    return dict(rv)
+    rv2 = defaultdict(list)
+    for k, vs in rv1.items():
+        for v in vs:
+            rv2[v].append(k)
+    return dict(rv2)
 
 
 def _count_front(s: str) -> int:
@@ -124,7 +133,7 @@ def _count_front(s: str) -> int:
             return position
 
 
-def get_interpro_to_proteins_df(version: str):
+def get_interpro_to_proteins_df(version: str, force: bool = False):
     """Get InterPro to Protein dataframe."""
     url = f"ftp://ftp.ebi.ac.uk/pub/databases/interpro/{version}/protein2ipr.dat.gz"
     df = ensure_df(
@@ -133,9 +142,11 @@ def get_interpro_to_proteins_df(version: str):
         compression="gzip",
         usecols=[0, 1, 3],
         names=INTERPRO_PROTEIN_COLUMNS,
+        version=version,
+        force=force,
     )
     return multisetdict((interpro_id, uniprot_id) for uniprot_id, interpro_id in df.values)
 
 
 if __name__ == "__main__":
-    get_obo().write_default()
+    get_obo().write_default(force=True, write_obo=True)
