b40386c2f695e3c4cbd6339fa81bd4874918b755
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-08-30 12:02:10 +0200

    Bump version: 0.6.3-dev → 0.6.3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,7 +55,7 @@ author = 'Charles Tapley Hoyt'
 #
 
 # The full version, including alpha/beta/rc tags.
-release = '0.6.3-dev'
+release = '0.6.3'
 
 # The short X.Y version.
 parsed_version = re.match(
