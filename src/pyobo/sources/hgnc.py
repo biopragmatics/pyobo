5ebfacc661b017fa4f16cc5ab9348ee6f2f987c7
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-09-15 02:10:16 +0200

    Update HGNC and HGNC gene family exports

--- src/pyobo/sources/hgnc.py
+++ src/pyobo/sources/hgnc.py
@@ -8,6 +8,7 @@ from typing import Iterable
 
 from tqdm import tqdm
 
+from ..api import get_name
 from ..struct import (
     Obo,
     Reference,
@@ -17,6 +18,7 @@ from ..struct import (
     from_species,
     gene_product_is_a,
     has_gene_product,
+    member_of,
     orthologous,
     transcribes_to,
 )
@@ -94,13 +96,21 @@ ENCODINGS = {
 }
 
 
-def get_obo() -> Obo:
+def get_obo(force: bool = False) -> Obo:
     """Get HGNC as OBO."""
     return Obo(
         ontology=PREFIX,
         name="HGNC",
         iter_terms=get_terms,
-        typedefs=[from_species, has_gene_product, gene_product_is_a, transcribes_to, orthologous],
+        iter_terms_kwargs=dict(force=force),
+        typedefs=[
+            from_species,
+            has_gene_product,
+            gene_product_is_a,
+            transcribes_to,
+            orthologous,
+            member_of,
+        ],
         synonym_typedefs=[
             previous_name_type,
             previous_symbol_type,
@@ -111,10 +121,10 @@ def get_obo() -> Obo:
     )
 
 
-def get_terms() -> Iterable[Term]:
+def get_terms(force: bool = False) -> Iterable[Term]:
     """Get HGNC terms."""
     unhandled = set()
-    path = ensure_path(PREFIX, url=DEFINITIONS_URL)
+    path = ensure_path(PREFIX, url=DEFINITIONS_URL, force=force)
     with open(path) as file:
         entries = json.load(file)["response"]["docs"]
 
@@ -125,34 +135,59 @@ def get_terms() -> Iterable[Term]:
             entry.pop("hgnc_id")[len("HGNC:") :],
         )
 
-        relations = []
+        term = Term(
+            definition=name,
+            reference=Reference(prefix=PREFIX, identifier=identifier, name=symbol),
+        )
+
         for uniprot_id in entry.pop("uniprot_ids", []):
-            relations.append((has_gene_product, Reference(prefix="uniprot", identifier=uniprot_id)))
+            term.append_relationship(
+                has_gene_product,
+                Reference(
+                    prefix="uniprot",
+                    identifier=uniprot_id,
+                    name=get_name(
+                        "uniprot",
+                        uniprot_id,
+                    ),
+                ),
+            )
         for ec_code in entry.pop("enzyme_id", []):
-            relations.append((gene_product_is_a, Reference(prefix="ec-code", identifier=ec_code)))
+            term.append_relationship(
+                gene_product_is_a,
+                Reference(prefix="ec-code", identifier=ec_code, name=get_name("ec-code", ec_code)),
+            )
         for rna_central_id in entry.pop("rna_central_id", []):
-            relations.append(
-                (transcribes_to, Reference(prefix="rnacentral", identifier=rna_central_id))
+            term.append_relationship(
+                transcribes_to, Reference(prefix="rnacentral", identifier=rna_central_id)
             )
         mirbase_id = entry.pop("mirbase", None)
         if mirbase_id:
-            relations.append((transcribes_to, Reference(prefix="mirbase", identifier=mirbase_id)))
+            term.append_relationship(
+                transcribes_to,
+                Reference(
+                    prefix="mirbase", identifier=mirbase_id, name=get_name("mirbase", mirbase_id)
+                ),
+            )
         snornabase_id = entry.pop("snornabase", None)
         if snornabase_id:
-            relations.append(
-                (transcribes_to, Reference(prefix="snornabase", identifier=snornabase_id))
+            term.append_relationship(
+                transcribes_to, Reference(prefix="snornabase", identifier=snornabase_id)
             )
 
-        for rgd_id in entry.pop("rgd_id", []):
-            relations.append(
-                (orthologous, Reference(prefix="rgd", identifier=rgd_id[len("RGD:") :]))
+        for rgd_curie in entry.pop("rgd_id", []):
+            rgd_id = rgd_curie[len("RGD:") :]
+            term.append_relationship(
+                orthologous,
+                Reference(prefix="rgd", identifier=rgd_id, name=get_name("rgd", rgd_id)),
             )
-        for mgi_id in entry.pop("mgd_id", []):
-            relations.append(
-                (orthologous, Reference(prefix="mgi", identifier=mgi_id[len("MGI:") :]))
+        for mgi_curie in entry.pop("mgd_id", []):
+            mgi_id = mgi_curie[len("MGI:") :]
+            term.append_relationship(
+                orthologous,
+                Reference(prefix="mgi", identifier=mgi_id, name=get_name("mgi", mgi_id)),
             )
 
-        xrefs = []
         for xref_prefix, key in gene_xrefs:
             xref_identifiers = entry.pop(key, None)
             if xref_identifiers is None:
@@ -160,45 +195,31 @@ def get_terms() -> Iterable[Term]:
             if not isinstance(xref_identifiers, list):
                 xref_identifiers = [xref_identifiers]
             for xref_identifier in xref_identifiers:
-                xrefs.append(Reference(prefix=xref_prefix, identifier=str(xref_identifier)))
+                term.append_xref(Reference(prefix=xref_prefix, identifier=str(xref_identifier)))
 
-        provenance = [
-            Reference(prefix="pubmed", identifier=str(pubmed_id))
-            for pubmed_id in entry.pop("pubmed_id", [])
-        ]
+        for pubmed_id in entry.pop("pubmed_id", []):
+            term.append_provenance(Reference(prefix="pubmed", identifier=str(pubmed_id)))
 
         gene_group_ids = entry.pop("gene_group_id", [])
         gene_groups = entry.pop("gene_group", [])
-        parents = [
-            Reference(
-                prefix="hgnc.genefamily",
-                identifier=str(hgncgenefamily_id),
-                name=gene_group_label,
+        for hgncgenefamily_id, gene_group_label in zip(gene_group_ids, gene_groups):
+            term.append_relationship(
+                member_of,
+                Reference(
+                    prefix="hgnc.genefamily",
+                    identifier=str(hgncgenefamily_id),
+                    name=gene_group_label,
+                ),
             )
-            for hgncgenefamily_id, gene_group_label in zip(gene_group_ids, gene_groups)
-        ]
 
-        synonyms = []
         for alias_symbol in entry.pop("alias_symbol", []):
-            synonyms.append(Synonym(name=alias_symbol, type=alias_symbol_type))
+            term.append_synonym(Synonym(name=alias_symbol, type=alias_symbol_type))
         for alias_name in entry.pop("alias_name", []):
-            synonyms.append(Synonym(name=alias_name, type=alias_name_type))
+            term.append_synonym(Synonym(name=alias_name, type=alias_name_type))
         for previous_symbol in entry.pop("previous_symbol", []):
-            synonyms.append(Synonym(name=previous_symbol, type=previous_symbol_type))
+            term.append_synonym(Synonym(name=previous_symbol, type=previous_symbol_type))
         for previous_name in entry.pop("prev_name", []):
-            synonyms.append(Synonym(name=previous_name, type=previous_name_type))
-
-        term = Term(
-            definition=name,
-            reference=Reference(prefix=PREFIX, identifier=identifier, name=symbol),
-            xrefs=xrefs,
-            provenance=provenance,
-            parents=parents,
-            synonyms=synonyms,
-        )
-
-        for typedef, reference in relations:
-            term.append_relationship(typedef, reference)
+            term.append_synonym(Synonym(name=previous_name, type=previous_name_type))
 
         for prop in ["locus_group", "locus_type", "location"]:
             value = entry.get(prop)
@@ -215,4 +236,4 @@ def get_terms() -> Iterable[Term]:
 
 
 if __name__ == "__main__":
-    get_obo().write_default()
+    get_obo(force=True).write_default(write_obo=True, force=True)
--- src/pyobo/sources/hgncgenefamily.py
+++ src/pyobo/sources/hgncgenefamily.py
@@ -20,21 +20,22 @@ HIERARCHY_URL = (
 symbol_type = SynonymTypeDef(id="symbol", name="symbol")
 
 
-def get_obo() -> Obo:
+def get_obo(force: bool = False) -> Obo:
     """Get HGNC Gene Families as OBO."""
     return Obo(
         ontology=PREFIX,
         name="HGNC Gene Families",
         iter_terms=get_terms,
+        iter_terms_kwargs=dict(force=force),
         synonym_typedefs=[symbol_type],
         typedefs=[from_species],
         auto_generated_by=f"bio2obo:{PREFIX}",
     )
 
 
-def get_hierarchy() -> Mapping[str, List[str]]:
+def get_hierarchy(force: bool = False) -> Mapping[str, List[str]]:
     """Get the HGNC Gene Families hierarchy as a dictionary."""
-    path = ensure_path(PREFIX, url=HIERARCHY_URL)
+    path = ensure_path(PREFIX, url=HIERARCHY_URL, force=force)
     df = pd.read_csv(path, dtype={"parent_fam_id": str, "child_fam_id": str})
     d = defaultdict(list)
     for parent_id, child_id in df.values:
@@ -45,10 +46,10 @@ def get_hierarchy() -> Mapping[str, List[str]]:
 COLUMNS = ["id", "abbreviation", "name", "pubmed_ids", "desc_comment", "desc_go"]
 
 
-def get_terms() -> Iterable[Term]:
+def get_terms(force: bool = False) -> Iterable[Term]:
     """Get the HGNC Gene Family terms."""
-    terms = list(_get_terms_helper())
-    hierarchy = get_hierarchy()
+    terms = list(_get_terms_helper(force=force))
+    hierarchy = get_hierarchy(force=force)
 
     id_to_term = {term.reference.identifier: term for term in terms}
     for child_id, parent_ids in hierarchy.items():
@@ -65,41 +66,29 @@ def get_terms() -> Iterable[Term]:
     return terms
 
 
-def _get_terms_helper() -> Iterable[Term]:
-    path = ensure_path(PREFIX, url=FAMILIES_URL)
+def _get_terms_helper(force: bool = False) -> Iterable[Term]:
+    path = ensure_path(PREFIX, url=FAMILIES_URL, force=force)
     df = pd.read_csv(path, dtype={"id": str})
 
     it = tqdm(df[COLUMNS].values, desc=f"Mapping {PREFIX}")
     for hgncgenefamily_id, symbol, name, pubmed_ids, definition, desc_go in it:
-        if pubmed_ids and pd.notna(pubmed_ids):
-            provenance = [
-                Reference(prefix="pubmed", identifier=s.strip()) for s in pubmed_ids.split(",")
-            ]
-        else:
-            provenance = []
-
         if not definition or pd.isna(definition):
-            definition = ""
-
-        xrefs = []
-        if desc_go and pd.notna(desc_go):
-            go_id = desc_go[len("http://purl.uniprot.org/go/") :]
-            xrefs.append(Reference(prefix="go", identifier=go_id))
-
-        synonyms = []
-        if symbol and pd.notna(symbol):
-            synonyms.append(Synonym(name=symbol, type=symbol_type))
-
+            definition = None
         term = Term(
             reference=Reference(prefix=PREFIX, identifier=hgncgenefamily_id, name=name),
             definition=definition,
-            provenance=provenance,
-            xrefs=xrefs,
-            synonyms=synonyms,
         )
+        if pubmed_ids and pd.notna(pubmed_ids):
+            for s in pubmed_ids.split(","):
+                term.append_provenance(Reference(prefix="pubmed", identifier=s.strip()))
+        if desc_go and pd.notna(desc_go):
+            go_id = desc_go[len("http://purl.uniprot.org/go/") :]
+            term.append_xref(Reference(prefix="go", identifier=go_id))
+        if symbol and pd.notna(symbol):
+            term.append_synonym(Synonym(name=symbol, type=symbol_type))
         term.set_species(identifier="9606", name="Homo sapiens")
         yield term
 
 
 if __name__ == "__main__":
-    get_obo().write_default()
+    get_obo(force=True).write_default(force=True, write_obo=True)
