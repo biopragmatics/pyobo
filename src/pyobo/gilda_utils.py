eb9ac2186358a1e6d70c1159008cd370b99d937e
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-10-05 17:04:59 +0200

    Update gilda_utils.py

--- src/pyobo/gilda_utils.py
+++ src/pyobo/gilda_utils.py
@@ -2,7 +2,7 @@
 
 """PyOBO's Gilda utilities."""
 
-from typing import Iterable, Optional, Tuple, Union
+from typing import Iterable, Optional, Tuple, Union, Type
 
 import bioregistry
 import gilda.api
@@ -80,7 +80,9 @@ def normalize_identifier(prefix: str, identifier: str) -> str:
 
 
 def get_grounder(
-    prefix: Union[str, Iterable[str]], unnamed: Optional[Iterable[str]] = None
+    prefix: Union[str, Iterable[str]],
+    unnamed: Optional[Iterable[str]] = None,
+    grounder_cls: Optional[Type[Grounder]] = None,
 ) -> Grounder:
     """Get a Gilda grounder for the given namespace."""
     unnamed = set() if unnamed is None else set(unnamed)
@@ -97,7 +99,9 @@ def get_grounder(
             terms.extend(p_terms)
     terms = filter_out_duplicates(terms)
     terms = multidict((term.norm_text, term) for term in terms)
-    return Grounder(terms)
+    if grounder_cls is None:
+        grounder_cls = Grounder
+    return grounder_cls(terms)
 
 
 def get_gilda_terms(prefix: str, identifiers_are_names: bool = False) -> Iterable[gilda.term.Term]:
