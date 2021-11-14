ab9be606978b4a10809dd5ad9e5695914f77f9b6
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-11-14 18:33:22 +0100

    Add class resolver

--- src/pyobo/sources/__init__.py
+++ src/pyobo/sources/__init__.py
@@ -1,3 +1,24 @@
 # -*- coding: utf-8 -*-
 
 """Sources of OBO content."""
+
+from class_resolver import Resolver
+
+from .cgnc import CGNCGetter
+from .chembl import ChEBMLGetter
+from .complexportal import ComplexPortalGetter
+from .umls import UMLSGetter
+from .uniprot import UniProtGetter
+from ..struct import Obo
+
+__all__ = [
+    "ontology_resolver",
+    # Getters
+    "CGNCGetter",
+    "ChEBMLGetter",
+    "ComplexPortalGetter",
+    "UMLSGetter",
+    "UniProtGetter",
+]
+
+ontology_resolver = Resolver.from_subclasses(base=Obo, suffix="Getter")
