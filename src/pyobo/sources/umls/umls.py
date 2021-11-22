efaeed69614ffc054af3c5be2de0310b0b0e7bf0
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-11-22 11:54:09 +0100

    Update UMLS download and version getter

--- src/pyobo/sources/umls/umls.py
+++ src/pyobo/sources/umls/umls.py
@@ -8,17 +8,14 @@ Run with ``python -m pyobo.sources.umls``
 import itertools as itt
 import operator
 import os
-import zipfile
-from pathlib import Path
 from typing import Iterable
 
 import pandas as pd
 from tqdm import tqdm
+from umls_downloader import open_umls
 
 from pyobo import Obo, Reference, Synonym, SynonymTypeDef, Term, normalize_prefix
-from pyobo.constants import RAW_MODULE
 from pyobo.utils.io import open_map_tsv
-from pyobo.utils.path import ensure_path
 
 __all__ = [
     "UMLSGetter",
@@ -53,23 +50,18 @@ PREFIX = "umls"
 
 SOURCE_VOCAB_URL = "https://www.nlm.nih.gov/research/umls/sourcereleasedocs/index.html"
 
-
-def _get_version() -> str:
-    return "2020AB"
-
-
 SYNONYM_ABB = open_map_tsv(SYNONYM_TYPE_PATH)
 
 
 class UMLSGetter(Obo):
     """An ontology representation of UMLS."""
 
-    ontology = PREFIX
+    ontology = bioversions_key = PREFIX
     synonym_typedefs = [SynonymTypeDef.from_text(v) for v in SYNONYM_ABB.values()]
 
     def iter_terms(self, force: bool = False) -> Iterable[Term]:
         """Iterate over terms in the ontology."""
-        return iter_terms(_get_version())
+        return iter_terms(version=self.data_version)
 
 
 def get_obo() -> Obo:
@@ -77,73 +69,59 @@ def get_obo() -> Obo:
     return UMLSGetter()
 
 
-def iter_terms(version: str, autodownload: bool = False) -> Iterable[Term]:
+def iter_terms(version: str) -> Iterable[Term]:
     """Iterate over UMLS terms."""
-    name = f"umls-{version}-mrconso.zip"
-    url = f"https://download.nlm.nih.gov/umls/kss/{version}/{name}"
-    if autodownload:
-        # FIXME needs automated scrapy step where you put in user/password
-        path = Path(ensure_path(PREFIX, url=url, version=version))
-    else:
-        path = RAW_MODULE.get(PREFIX, version, name=name)
-        if not path.exists():
-            raise FileNotFoundError(
-                f"UMLS needs to be downloaded manually still and moved to  {path}. "
-                f"See https://www.nlm.nih.gov/research/umls/index.html",
+    with open_umls(version=version) as file:
+        it = tqdm(file, unit_scale=True, desc="[umls] parsing")
+        lines = (line.decode("utf-8").strip().split("|") for line in it)
+        for cui, cui_lines in itt.groupby(lines, key=operator.itemgetter(0)):
+            df = pd.DataFrame(list(cui_lines), columns=RRF_COLUMNS)
+            df = df[df["LAT - Language"] == "ENG"]
+            idx = (
+                (df["ISPREF - is preferred"] == "Y")
+                & (df["TS - Term Status"] == "P")
+                & (df["STT - String Type"] == "PF"),
+            )
+            pref_rows_df = df.loc[idx]
+            if len(pref_rows_df.index) != 1:
+                it.write(f"no preferred term for umls:{cui}. got {len(pref_rows_df.index)}")
+                continue
+
+            df["TTY - Term Type in Source"] = df["TTY - Term Type in Source"].map(
+                SYNONYM_ABB.__getitem__
             )
 
-    with zipfile.ZipFile(path) as zip_file:
-        with zip_file.open("MRCONSO.RRF", mode="r") as file:
-            it = tqdm(file, unit_scale=True, desc="[umls] parsing")
-            lines = (line.decode("utf-8").strip().split("|") for line in it)
-            for cui, cui_lines in itt.groupby(lines, key=operator.itemgetter(0)):
-                df = pd.DataFrame(list(cui_lines), columns=RRF_COLUMNS)
-                df = df[df["LAT - Language"] == "ENG"]
-                idx = (
-                    (df["ISPREF - is preferred"] == "Y")
-                    & (df["TS - Term Status"] == "P")
-                    & (df["STT - String Type"] == "PF"),
-                )
-                pref_rows_df = df.loc[idx]
-                if len(pref_rows_df.index) != 1:
-                    it.write(f"no preferred term for umls:{cui}. got {len(pref_rows_df.index)}")
-                    continue
+            _r = pref_rows_df.iloc[0]
+            sdf = df[["SAB - source name", "CODE", "TTY - Term Type in Source", "STR"]]
 
-                df["TTY - Term Type in Source"] = df["TTY - Term Type in Source"].map(
-                    SYNONYM_ABB.__getitem__
-                )
-
-                _r = pref_rows_df.iloc[0]
-                sdf = df[["SAB - source name", "CODE", "TTY - Term Type in Source", "STR"]]
-
-                synonyms = []
-                xrefs = []
-                for source, identifier, synonym_type, synonym in sdf.values:
-                    norm_source = normalize_prefix(source)
-                    if norm_source is None or not identifier:
-                        provenance = []
-                    else:
-                        ref = Reference(prefix=norm_source, identifier=identifier)
-                        provenance = [ref]
-                        xrefs.append(ref)
-                    synonyms.append(
-                        Synonym(
-                            name=synonym,
-                            provenance=provenance,
-                            type=SynonymTypeDef.from_text(synonym_type),
-                        )
+            synonyms = []
+            xrefs = []
+            for source, identifier, synonym_type, synonym in sdf.values:
+                norm_source = normalize_prefix(source)
+                if norm_source is None or not identifier:
+                    provenance = []
+                else:
+                    ref = Reference(prefix=norm_source, identifier=identifier)
+                    provenance = [ref]
+                    xrefs.append(ref)
+                synonyms.append(
+                    Synonym(
+                        name=synonym,
+                        provenance=provenance,
+                        type=SynonymTypeDef.from_text(synonym_type),
                     )
-
-                xrefs = sorted(
-                    set(xrefs), key=lambda reference: (reference.prefix, reference.identifier)
                 )
 
-                term = Term(
-                    reference=Reference(prefix=PREFIX, identifier=cui, name=_r["STR"]),
-                    synonyms=synonyms,
-                    xrefs=xrefs,
-                )
-                yield term
+            xrefs = sorted(
+                set(xrefs), key=lambda reference: (reference.prefix, reference.identifier)
+            )
+
+            term = Term(
+                reference=Reference(prefix=PREFIX, identifier=cui, name=_r["STR"]),
+                synonyms=synonyms,
+                xrefs=xrefs,
+            )
+            yield term
 
 
 if __name__ == "__main__":
