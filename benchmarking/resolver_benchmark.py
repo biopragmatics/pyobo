9bc3ae4e94d294b2bdca828be062b9ffee5a9cad
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-11-21 15:26:13 +0100

    Update black config

--- benchmarking/resolver_benchmark.py
+++ benchmarking/resolver_benchmark.py
@@ -14,27 +14,24 @@ from tqdm import tqdm
 import pyobo
 
 
-def _get_urls(prefix='doid', host='localhost', port=5000):
+def _get_urls(prefix="doid", host="localhost", port=5000):
     identifiers = pyobo.get_id_name_mapping(prefix)
-    return [
-        f'http://{host}:{port}/resolve/{prefix}:{identifier}'
-        for identifier in identifiers
-    ]
+    return [f"http://{host}:{port}/resolve/{prefix}:{identifier}" for identifier in identifiers]
 
 
-def benchmark_sync(prefix='doid', host='localhost', port=5000):
+def benchmark_sync(prefix="doid", host="localhost", port=5000):
     urls = _get_urls(prefix=prefix, host=host, port=port)
 
     start = time.time()
-    for url in tqdm(urls, desc=f'benchmarking {prefix}'):
+    for url in tqdm(urls, desc=f"benchmarking {prefix}"):
         res = requests.get(url)
         res.raise_for_status()
     elapsed = time.time() - start
     avg_elapsed = len(urls) / elapsed
-    print(f'Made {len(urls)} requests in {elapsed:.2f} seconds. Avg = {avg_elapsed:.2f} requests/s')
+    print(f"Made {len(urls)} requests in {elapsed:.2f} seconds. Avg = {avg_elapsed:.2f} requests/s")
 
 
-def benchmark_async(prefix='doid', host='localhost', port=5000):
+def benchmark_async(prefix="doid", host="localhost", port=5000):
     urls = _get_urls(prefix=prefix, host=host, port=port)
 
     p = pool.Pool.from_urls(urls)
@@ -42,22 +39,24 @@ def benchmark_async(prefix='doid', host='localhost', port=5000):
     start = time.time()
 
     # The following code is a modified version of ``p.join_all()`` with tqdm
-    for session_thread in tqdm(p._pool, desc=f'benchmarking async {prefix}'):
+    for session_thread in tqdm(p._pool, desc=f"benchmarking async {prefix}"):
         session_thread.join()
 
     elapsed = time.time() - start
     avg_elapsed = len(urls) / elapsed
-    print(f'Made {len(urls)} async requests in {elapsed:.2f} seconds. Avg = {avg_elapsed:.2f} requests/s')
+    print(
+        f"Made {len(urls)} async requests in {elapsed:.2f} seconds. Avg = {avg_elapsed:.2f} requests/s"
+    )
 
 
 @click.command()
-@click.option('--prefix', default='doid')
-@click.option('--host', default='localhost')
-@click.option('--port', default='5000')
+@click.option("--prefix", default="doid")
+@click.option("--host", default="localhost")
+@click.option("--port", default="5000")
 def main(prefix, host, port):
     benchmark_sync(prefix=prefix, host=host, port=port)
     benchmark_async(prefix=prefix, host=host, port=port)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
