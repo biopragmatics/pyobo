1b6450a713dd05b3d364084addb9d3cdfddd5f80
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-02-21 10:53:16 +0100

    Handle CVX obsolete/replaced/placeholder entries (#171)
    * Skip CVX obsolete/placeholder entries
    
    * Add replacement info

--- src/pyobo/sources/cvx.py
+++ src/pyobo/sources/cvx.py
@@ -7,7 +7,7 @@ from typing import Iterable
 
 import pandas as pd
 
-from pyobo import Obo, Term
+from pyobo import Obo, Reference, Term
 
 __all__ = [
     "CVXGetter",
@@ -26,6 +26,11 @@ class CVXGetter(Obo):
     def iter_terms(self, force: bool = False) -> Iterable[Term]:
         """Iterate over terms in the ontology."""
         return iter_terms()
+
+
+# This got split, which it's not obvious how to deal with this
+MANUAL_OBSOLETE = {"15"}
+REPLACEMENTS = {"31": "85", "154": "86", "180": "13"}
 
 
 def iter_terms() -> Iterable[Term]:
@@ -60,11 +65,22 @@ def iter_terms() -> Iterable[Term]:
         cvx_df[col] = cvx_df[col].map(lambda s: s.strip() if pd.notna(s) else s)
     terms = {}
     for cvx, short_name, full_name, notes, status, nonvaccine, _updated in cvx_df.values:
-        term = Term.from_triple(PREFIX, cvx, full_name)
+        if cvx == "99":
+            continue  # this is a placeholder
+
+        is_obsolete = cvx in MANUAL_OBSOLETE or (pd.notna(notes) and "do not use" in notes.lower())
+        term = Term(
+            reference=Reference(prefix=PREFIX, identifier=cvx, name=full_name),
+            is_obsolete=is_obsolete,
+        )
         if short_name != full_name:
             term.append_synonym(short_name)
         if pd.notna(notes):
             term.append_comment(notes)
+        if is_obsolete:
+            replacement_identifier = REPLACEMENTS.get(cvx)
+            if replacement_identifier:
+                term.append_replaced_by(Reference(prefix=PREFIX, identifier=replacement_identifier))
         if pd.notna(status):
             term.append_property("status", status)
         if pd.notna(nonvaccine):
