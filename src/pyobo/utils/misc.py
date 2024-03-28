6587f01eb030d479555849faac7f6640bb6f4d57
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-03-28 11:09:52 +0100

    Update version parsing

--- src/pyobo/utils/misc.py
+++ src/pyobo/utils/misc.py
@@ -52,22 +52,28 @@ def cleanup_version(data_version: str, prefix: str) -> Optional[str]:
     if prefix == "orth":
         # TODO add bioversions for this
         return "2"
-    if data_version.startswith("http://www.orpha.net/version"):
-        return data_version[len("http://www.orpha.net/version") :]
-    if data_version.startswith("http://humanbehaviourchange.org/ontology/bcio.owl/"):
-        return data_version[len("http://humanbehaviourchange.org/ontology/bcio.owl/") :]
-    if data_version.startswith("http://www.ebi.ac.uk/efo/releases/v"):
-        return data_version[len("http://www.ebi.ac.uk/efo/releases/v") :].split("/")[0]
-    if data_version.startswith("http://www.ebi.ac.uk/swo/swo.owl/"):
-        return data_version[len("http://www.ebi.ac.uk/swo/swo.owl/") :].split("/")[0]
-    if data_version.startswith("http://purl.org/pav/"):
-        return data_version[len("http://purl.org/pav/") :]
-    if data_version.startswith("http://semanticscience.org/ontology/sio/v"):
-        return data_version[len("http://semanticscience.org/ontology/sio/v")].split("/")[0]
-    if data_version.startswith("http://ontology.neuinfo.org/NIF/ttl/nif/version/"):
-        return data_version[len("http://ontology.neuinfo.org/NIF/ttl/nif/version/") :].split("/")[0]
-    if data_version.startswith("http://identifiers.org/combine.specifications/teddy.rel-"):
-        return data_version[len("http://identifiers.org/combine.specifications/teddy.rel-") :]
+
+    version_prefixes = [
+        "http://www.orpha.net/version",
+        "https://www.orphadata.com/data/ontologies/ordo/last_version/ORDO_en_",
+        "http://humanbehaviourchange.org/ontology/bcio.owl/",
+        "http://purl.org/pav/",
+        "http://identifiers.org/combine.specifications/teddy.rel-",
+    ]
+    for version_prefix in version_prefixes:
+        if data_version.startswith(version_prefix):
+            return data_version[len(version_prefix) :]
+
+    version_prefixes_split = [
+        "http://www.ebi.ac.uk/efo/releases/v",
+        "http://www.ebi.ac.uk/swo/swo.owl/",
+        "http://semanticscience.org/ontology/sio/v",
+        "http://ontology.neuinfo.org/NIF/ttl/nif/version/",
+    ]
+    for version_prefix_split in version_prefixes_split:
+        if data_version.startswith(version_prefix_split):
+            return data_version[len(version_prefix_split) :].split("/")[0]
+
     if data_version.replace(".", "").isnumeric():
         return data_version  # consecutive, major.minor, or semantic versioning
     for v in reversed(data_version.split("/")):
