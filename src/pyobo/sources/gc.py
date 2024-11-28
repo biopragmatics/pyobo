2485716e2bc681af350847d71dafb39ebc462131
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-11-28 17:12:19 +0100

    Add non-isomorphic ontologization

--- src/pyobo/sources/gc.py
+++ src/pyobo/sources/gc.py
@@ -6,6 +6,7 @@
 from collections.abc import Iterable
 
 from pyobo.struct import Obo, Reference, Term, TypeDef, int_identifier_sort_key
+from pyobo.struct.typedef import contributor
 from pyobo.utils.path import ensure_path
 
 PREFIX = "gc"
@@ -15,15 +16,33 @@ URI_PREFIX = (
 URL = "ftp://ftp.ncbi.nih.gov/entrez/misc/data/gc.prt"
 VERSION = "4.6"
 
-ROOT = Reference(prefix=PREFIX, identifier="0", name="genetic code")
-TAX_ROOT = Reference(prefix="NCBITaxon", identifier="1", name="root")
+GC_ROOT = Reference(prefix=PREFIX, identifier="0", name="genetic code")
+NCBITAXON_ROOT = Reference(prefix="NCBITaxon", identifier="1", name="root")
 
 has_gc_code = TypeDef(
     reference=Reference(prefix=PREFIX, identifier="1000000", name="has GC code"),
     definition="Connects a taxonomy term to a GC code",
-    domain=TAX_ROOT,
-    range=ROOT,
+    domain=NCBITAXON_ROOT,
+    range=GC_ROOT,
 )
+CHARLIE = Reference(prefix="orcid", identifier="0000-0003-4423-4370", name="Charles Tapley Hoyt")
+
+NUCLEAR_GENETIC_CODE = Reference(prefix=PREFIX, identifier="2000001", name="nuclear genetic code")
+MITOCHONDRIAL_GENETIC_CODE = Reference(
+    prefix=PREFIX, identifier="2000002", name="mitochondrial genetic code"
+)
+PLASTID_GENETIC_CODE = Reference(prefix=PREFIX, identifier="2000003", name="plastid genetic code")
+MACRONUCLEAR_GENETIC_CODE = Reference(
+    prefix=PREFIX, identifier="2000004", name="macronuclear genetic code"
+)
+
+CHILDREN = {
+    NUCLEAR_GENETIC_CODE: [12, 31, 6, 28, 10, 27, 29, 26, 30],
+    MITOCHONDRIAL_GENETIC_CODE: [14, 13, 16, 9, 5, 4, 22, 23, 21, 2, 3, 24],
+    PLASTID_GENETIC_CODE: [11, 32],
+    MACRONUCLEAR_GENETIC_CODE: [15],
+}
+XY = {str(value): key for key, values in CHILDREN.items() for value in values}
 
 
 class GCGetter(Obo):
@@ -32,9 +51,13 @@ class GCGetter(Obo):
     ontology = PREFIX
     static_version = VERSION
     term_sort_key = int_identifier_sort_key
-    root_terms = [ROOT]
+    root_terms = [GC_ROOT]
     typedefs = [has_gc_code]
-    idspaces = {PREFIX: URI_PREFIX}
+    idspaces = {
+        PREFIX: URI_PREFIX,
+        "orcid": "https://orcid.org/",
+        "dcterms": "http://purl.org/dc/terms/",
+    }
 
     def iter_terms(self, force: bool = False) -> Iterable[Term]:
         """Iterate over terms in the ontology."""
@@ -72,10 +95,20 @@ def get_terms() -> Iterable[Term]:
             elif key == "id":
                 entry["identifier"] = data.rstrip(",").rstrip()
 
-    terms = [Term(reference=ROOT)]
+    terms = [
+        Term(reference=GC_ROOT),
+        Term(reference=NCBITAXON_ROOT),
+    ]
+    for reference in CHILDREN:
+        term = Term(reference=reference)
+        term.append_parent(GC_ROOT)
+        term.annotate_object(contributor, CHARLIE)
+        terms.append(term)
+
     for entry in entries:
-        term = Term.from_triple(PREFIX, entry["identifier"], entry["name"])
-        term.append_parent(ROOT)
+        identifier = entry["identifier"]
+        term = Term.from_triple(PREFIX, identifier, entry["name"])
+        term.append_parent(XY.get(identifier, GC_ROOT))
         # TODO if symbol is available, what does it mean?
         terms.append(term)
 
