0c9397c0a150a73ef3b7db2e8c714955bc270366
Charles Tapley Hoyt <cthoyt@gmail.com>
2024-12-02 11:39:51 +0100

    Update gc.py

--- src/pyobo/sources/gc.py
+++ src/pyobo/sources/gc.py
@@ -16,7 +16,7 @@ URI_PREFIX = (
 URL = "ftp://ftp.ncbi.nih.gov/entrez/misc/data/gc.prt"
 VERSION = "4.6"
 
-GC_ROOT = Reference(prefix=PREFIX, identifier="0", name="genetic code")
+GC_ROOT = Reference(prefix=PREFIX, identifier="0", name="genetic code translation table")
 NCBITAXON_ROOT = Reference(prefix="NCBITaxon", identifier="1", name="root")
 
 has_gc_code = TypeDef(
@@ -27,22 +27,32 @@ has_gc_code = TypeDef(
 )
 CHARLIE = Reference(prefix="orcid", identifier="0000-0003-4423-4370", name="Charles Tapley Hoyt")
 
-NUCLEAR_GENETIC_CODE = Reference(prefix=PREFIX, identifier="2000001", name="nuclear genetic code")
+NUCLEAR_GENETIC_CODE = Reference(
+    prefix=PREFIX, identifier="2000001", name="nuclear genetic code translation table"
+)
 MITOCHONDRIAL_GENETIC_CODE = Reference(
-    prefix=PREFIX, identifier="2000002", name="mitochondrial genetic code"
+    prefix=PREFIX, identifier="2000002", name="mitochondrial genetic code translation table"
 )
-PLASTID_GENETIC_CODE = Reference(prefix=PREFIX, identifier="2000003", name="plastid genetic code")
-MACRONUCLEAR_GENETIC_CODE = Reference(
-    prefix=PREFIX, identifier="2000004", name="macronuclear genetic code"
+PLASTID_GENETIC_CODE = Reference(
+    prefix=PREFIX, identifier="2000003", name="plastid genetic code translation table"
 )
+NUCLEUS = Reference(prefix="GO", identifier="0005634", name="nucleus")
+MITOCHONDIA = Reference(prefix="GO", identifier="0005739", name="mitochondrion")
+PLASTID = Reference(prefix="GO", identifier="0009536", name="plastid")
 
-CHILDREN = {
-    NUCLEAR_GENETIC_CODE: [12, 31, 6, 28, 10, 27, 29, 26, 30],
+CATEGORY_TO_CELLULAR_COMPONENT = {
+    NUCLEAR_GENETIC_CODE: NUCLEUS,
+    MITOCHONDRIAL_GENETIC_CODE: MITOCHONDIA,
+    PLASTID_GENETIC_CODE: PLASTID,
+}
+CATEGORY_TO_TABLES = {
+    NUCLEAR_GENETIC_CODE: [12, 31, 6, 28, 10, 27, 29, 26, 30, 15],
     MITOCHONDRIAL_GENETIC_CODE: [14, 13, 16, 9, 5, 4, 22, 23, 21, 2, 3, 24],
     PLASTID_GENETIC_CODE: [11, 32],
-    MACRONUCLEAR_GENETIC_CODE: [15],
 }
-XY = {str(value): key for key, values in CHILDREN.items() for value in values}
+TABLE_TO_CATEGORY = {
+    str(value): key for key, values in CATEGORY_TO_TABLES.items() for value in values
+}
 
 
 class GCGetter(Obo):
@@ -96,19 +106,25 @@ def get_terms() -> Iterable[Term]:
                 entry["identifier"] = data.rstrip(",").rstrip()
 
     terms = [
-        Term(reference=GC_ROOT),
+        Term(
+            reference=GC_ROOT,
+            definition="A table for translating codons into amino acids. This can change for "
+            "different taxa, or be different in different organelles that include genetic information.",
+        ).append_provenance(CHARLIE),
         Term(reference=NCBITAXON_ROOT),
     ]
-    for reference in CHILDREN:
+    for reference in CATEGORY_TO_TABLES:
         term = Term(reference=reference)
         term.append_parent(GC_ROOT)
         term.annotate_object(contributor, CHARLIE)
+        if substructure := CATEGORY_TO_CELLULAR_COMPONENT.get(reference):
+            term.append_see_also(substructure)
         terms.append(term)
 
     for entry in entries:
         identifier = entry["identifier"]
         term = Term.from_triple(PREFIX, identifier, entry["name"])
-        term.append_parent(XY.get(identifier, GC_ROOT))
+        term.append_parent(TABLE_TO_CATEGORY.get(identifier, GC_ROOT))
         # TODO if symbol is available, what does it mean?
         terms.append(term)
 
@@ -128,6 +144,9 @@ def get_terms() -> Iterable[Term]:
         .append_replaced_by(Reference(prefix=PREFIX, identifier="1"))
         .append_comment("all plant chloroplast differences due to RNA edit, as of 1995.")
     )
+
+    for cellular_component in CATEGORY_TO_CELLULAR_COMPONENT.values():
+        terms.append(Term(reference=cellular_component))
 
     return terms
 
