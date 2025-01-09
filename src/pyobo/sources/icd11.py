2bc851c6ab05fa275a65ed5c0612612e7f0c75e9
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-01-09 11:09:51 -0500

    Import ICD11 MMS codes (#285)
    This PR adds ICD11 MMS codes as ad-hoc properties. Later, it can create
    proper mappings once a prefix is decided for the codes.
    
    Note that not all ICD11 identifiers have an MMS code

--- src/pyobo/sources/icd11.py
+++ src/pyobo/sources/icd11.py
@@ -5,19 +5,20 @@ Run with python -m pyobo.sources.icd11 -v
 
 import json
 import logging
-import os
 from collections.abc import Iterable, Mapping
 from typing import Any
 
 from tqdm.auto import tqdm
 
-from ..sources.icd_utils import (
+from .icd_utils import (
     ICD11_TOP_LEVEL_URL,
+    ICDError,
     get_child_identifiers,
     get_icd,
+    get_icd_11_mms,
     visiter,
 )
-from ..struct import Obo, Reference, Synonym, Term
+from ..struct import Obo, Reference, Synonym, Term, TypeDef, default_reference
 from ..utils.path import prefix_directory_join
 
 __all__ = [
@@ -28,11 +29,14 @@ logger = logging.getLogger(__name__)
 
 PREFIX = "icd11"
 
+CODE_PROP = TypeDef(reference=default_reference(PREFIX, "icd_mms_code"), is_metadata_tag=True)
+
 
 class ICD11Getter(Obo):
     """An ontology representation of ICD-11."""
 
     ontology = PREFIX
+    typedefs = [CODE_PROP]
     dynamic_version = True
 
     def iter_terms(self, force: bool = False) -> Iterable[Term]:
@@ -41,6 +45,35 @@ class ICD11Getter(Obo):
 
 
 def iterate_icd11() -> Iterable[Term]:
+    """Iterate over the terms in ICD11 and enrich them with MMS."""
+    res = get_icd(ICD11_TOP_LEVEL_URL)
+    res_json = res.json()
+    version = res_json["releaseId"]
+    mms_directory = prefix_directory_join(PREFIX, "mms", version=version)
+    terms = list(iterate_icd11_helper(res_json, version))
+    # this takes a bit more than 2 hours
+    for term in tqdm(terms, desc="Getting MMS", unit_scale=True):
+        path = mms_directory.joinpath(term.identifier).with_suffix(".json")
+        if path.exists():
+            mms_data = json.loads(path.read_text())
+        else:
+            try:
+                mms_data = get_icd_11_mms(term.identifier)
+            except ICDError:
+                # writing this isn't necessary since not all terms have MMS entries
+                # tqdm.write(str(e))
+                mms_data = {}
+            else:
+                path.write_text(json.dumps(mms_data))
+
+        if code := mms_data.get("code"):
+            # TODO decide on ICD code prefix, then append this as a mapping
+            term.annotate_literal(CODE_PROP, code)
+
+        yield term
+
+
+def iterate_icd11_helper(res_json, version) -> Iterable[Term]:
     """Iterate over the terms in ICD11.
 
     The API doesn't seem to have a rate limit, but returns pretty slow.
@@ -48,13 +81,9 @@ def iterate_icd11() -> Iterable[Term]:
     Get ready to be patient - the API token expires every hour so there's
     a caching mechanism with :mod:`cachier` that gets a new one every hour.
     """
-    res = get_icd(ICD11_TOP_LEVEL_URL)
-    res_json = res.json()
-
-    version = res_json["releaseId"]
-    directory = prefix_directory_join(PREFIX, version=version)
-
-    with open(os.path.join(directory, "top.json"), "w") as file:
+    directory = prefix_directory_join(PREFIX, "base", version=version)
+    top_path = directory.joinpath("top.json")
+    with top_path.open("w") as file:
         json.dump(res_json, file, indent=2)
 
     tqdm.write(f'There are {len(res_json["child"])} top level entities')
