f150d6bcc07b494590e91315caa0c26ade410c93
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-01-13 23:17:24 +0100

    Update PID source

--- src/pyobo/sources/pid.py
+++ src/pyobo/sources/pid.py
@@ -88,7 +88,7 @@ def iter_terms(force: bool = False) -> Iterable[Term]:
                 logger.debug(f"unmapped: {name}, {reference}")
 
         for hgnc_id, hgnc_symbol in genes:
-            term.append_relationship(
+            term.annotate_object(
                 has_participant, Reference(prefix="hgnc", identifier=hgnc_id, name=hgnc_symbol)
             )
 
