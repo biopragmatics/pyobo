52824d1e8406b80d47eea906d6c48704e426d99c
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-01-13 21:40:15 +0100

    Add grouping terms

--- src/pyobo/sources/umls/sty.py
+++ src/pyobo/sources/umls/sty.py
@@ -2,7 +2,7 @@
 
 from collections.abc import Iterable
 
-from pyobo import Obo, Reference, Term
+from pyobo import Obo, Reference, Term, default_reference
 from pyobo.struct.typedef import has_category
 from pyobo.utils.path import ensure_df
 
@@ -27,13 +27,29 @@ class UMLSSTyGetter(Obo):
         return iter_terms(version=self._version_or_raise)
 
 
+COLUMNS = [
+    "group",
+    "group_label",
+    "sty_id",
+    "sty_name",
+]
+
+
 def iter_terms(version: str) -> Iterable[Term]:
     """Iterate over UMLS terms."""
-    df = ensure_df(PREFIX, url=URL, version=version, sep="|")
+    df = ensure_df(PREFIX, url=URL, version=version, sep="|", header=None, names=COLUMNS)
+
+    extras = {
+        group: Term(
+            reference=default_reference(PREFIX, group, name=group_label),
+        )
+        for group, group_label in df[["group", "group_label"]].drop_duplicates().values
+    }
+    yield from extras.values()
+
     for group, _group_label, sty_id, sty_name in df.values:
-        term = Term(
-            reference=Reference(prefix="sty", identifier=sty_id, name=sty_name)
-        ).annotate_literal(has_category, group)
+        term = Term(reference=Reference(prefix="sty", identifier=sty_id, name=sty_name))
+        term.append_parent(extras[group])
         yield term
 
 
