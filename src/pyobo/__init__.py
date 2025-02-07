bf0bbb750b6e96dc12529cb3cd174798a94fde0a
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-02-07 12:50:26 +0100

    Update

--- src/pyobo/__init__.py
+++ src/pyobo/__init__.py
@@ -55,6 +55,7 @@ from .api import (
     is_descendent,
 )
 from .getters import get_ontology
+from .ner import get_grounder, literal_mappings_to_gilda
 from .ner.normalizer import OboNormalizer, ground
 from .obographs import parse_results_from_obo
 from .plugins import (
@@ -97,6 +98,7 @@ __all__ = [
     "get_filtered_relations_df",
     "get_filtered_xrefs",
     "get_graph",
+    "get_grounder",
     "get_hierarchy",
     "get_id_definition_mapping",
     "get_id_multirelations_mapping",
@@ -137,13 +139,13 @@ __all__ = [
     "get_xrefs",
     "get_xrefs_df",
     "ground",
-    "ground",
     "has_ancestor",
     "has_nomenclature_plugin",
     "has_xref_plugin",
     "is_descendent",
     "iter_nomenclature_plugins",
     "iter_xref_plugins",
+    "literal_mappings_to_gilda",
     "parse_results_from_obo",
     "run_nomenclature_plugin",
     "run_xref_plugin",
