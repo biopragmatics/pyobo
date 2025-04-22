ab38c3412034d3a1345f42759d218c296545a168
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-04-22 12:39:27 +0200

    Cleanup

--- src/pyobo/ner/normalizer.py
+++ src/pyobo/ner/normalizer.py
@@ -4,7 +4,10 @@ from __future__ import annotations
 
 from collections.abc import Iterable
 
+from typing_extensions import Unpack
+
 from .api import get_grounder
+from ..constants import GetOntologyKwargs
 from ..struct import Reference
 
 __all__ = [
@@ -12,22 +15,19 @@ __all__ = [
 ]
 
 
-def ground(prefix: str | Iterable[str], query: str) -> Reference | None:
+def ground(
+    prefix: str | Iterable[str], query: str, **kwargs: Unpack[GetOntologyKwargs]
+) -> Reference | None:
     """Normalize a string given the prefix's labels and synonyms.
 
     :param prefix: If a string, only grounds against that namespace. If a list, will try grounding
         against all in that order
     :param query: The string to try grounding
     """
-    if isinstance(prefix, str):
-        normalizer = get_grounder(prefix)
-        match = normalizer.get_best_match(query)
-        if match:
-            # TODO when generics are working, the grounder
-            #  can be type annotated with the right reference
-            return Reference.from_reference(match.reference)
-    else:
-        for p in prefix:
-            if rv := ground(p, query):
-                return rv
+    grounder = get_grounder(prefix, **kwargs)
+    match = grounder.get_best_match(query)
+    if match:
+        # TODO when generics are working, the grounder
+        #  can be type annotated with the right reference
+        return Reference.from_reference(match.reference)
     return None
