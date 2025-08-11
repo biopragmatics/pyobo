285eb252f071d4f6035bedcfdfd2ab3945390cfa
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-08-11 22:34:02 +0200

    Update cordis.py

--- src/pyobo/sources/cordis.py
+++ src/pyobo/sources/cordis.py
@@ -1,3 +1,38 @@
+"""Converter for CORDIS Projects."""
+
+from collections.abc import Iterable
+
+from pyobo import Obo, Reference, Term
+from pyobo.utils.path import ensure_path
+from pystow.utils import read_zipfile_csv
+
+__all__ = [
+    "CordisProjectGetter",
+]
+
 URL = "https://cordis.europa.eu/data/cordis-h2020projects-csv.zip"
+PREFIX = "cordis.project"
 
 # see euscivoc, which is in skosxl format
+
+
+class CordisProjectGetter(Obo):
+    """An ontology representation of cordis projects."""
+
+    ontology = PREFIX
+    dynamic_version = True
+
+    def iter_terms(self, force: bool = False) -> Iterable[Term]:
+        """Iterate over terms in the ontology."""
+        return iter_terms()
+
+
+def iter_terms() -> Iterable[Term]:
+    """Iterate over CPT terms."""
+    path = ensure_path("cordis", url=URL)
+    df = read_zipfile_csv(path, "project.csv", sep='\t')
+    print(df.head())
+
+
+if __name__ == "__main__":
+    CordisProjectGetter.cli()
