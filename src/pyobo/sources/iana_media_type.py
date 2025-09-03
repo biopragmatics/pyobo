4da33c497f97845a5c9902f9cb745519f2cca571
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-09-03 12:35:49 +0200

    Add classes for

--- src/pyobo/sources/iana_media_type.py
+++ src/pyobo/sources/iana_media_type.py
@@ -10,13 +10,13 @@ import requests
 from pyobo import Obo, Reference, Term, TypeDef, default_reference
 from pyobo.struct import Annotation
 from pyobo.struct.typedef import has_source, term_replaced_by
-from pyobo.struct.vocabulary import xsd_string
 from pyobo.utils.path import ensure_df
 
 __all__ = ["IANAGetter"]
 
 PREFIX = "iana.mediatype"
-ROOT = Term.from_triple(prefix="dcterms", identifier="MediaType", name="media type")
+ROOT_MEDIA_TYPE = Term.from_triple(prefix="dcterms", identifier="MediaType", name="media type")
+ROOT_FILE_FORMAT = Term.from_triple(prefix="dcterms", identifier="FileFormat", name="file format")
 
 #: The top-level types listed on https://www.iana.org/assignments/media-types/media-types.xhtml
 MEDIA_TYPE_GROUPS = [
@@ -37,7 +37,7 @@ GROUP_TO_CSV = {
         f"https://www.iana.org/assignments/media-types/{media_type_group}.csv",
         Term(
             reference=Reference(prefix=PREFIX, identifier=media_type_group, name=media_type_group)
-        ).append_parent(ROOT),
+        ).append_parent(ROOT_MEDIA_TYPE),
     )
     for media_type_group in MEDIA_TYPE_GROUPS
 }
@@ -58,11 +58,11 @@ def _get_mimetypes():
 
 PREDICATE = TypeDef(
     reference=default_reference(
-        prefix=PREFIX, identifier="hasExtension", name="media type uses file extension"
+        prefix=PREFIX, identifier="extension", name="appears with file extension"
     ),
-    domain=ROOT.reference,
-    range=xsd_string,
-    definition="Connects a media type to a string representing the file extension",
+    domain=ROOT_MEDIA_TYPE.reference,
+    range=ROOT_FILE_FORMAT.reference,
+    definition="Connects a media type with a file format that has been observed to encode it",
     is_metadata_tag=True,
 )
 
@@ -73,7 +73,7 @@ class IANAGetter(Obo):
     ontology = bioregistry_key = PREFIX
     name = "IANA Media Types"
     dynamic_version = True
-    root_terms = [t.reference for _, (_, t) in sorted(GROUP_TO_CSV.items())]
+    root_terms = [ROOT_MEDIA_TYPE.reference, ROOT_FILE_FORMAT.reference]
     typedefs = [
         term_replaced_by,
         PREDICATE,
@@ -84,9 +84,22 @@ class IANAGetter(Obo):
         return get_terms()
 
 
-def get_terms() -> list[Term]:
+def get_terms() -> Iterable[Term]:
     """Get IANA Media Type terms."""
     mimetypes_data = _get_mimetypes()
+
+    filetype_to_term = {}
+    for record in mimetypes_data.values():
+        for filetype_str in record.get("fileTypes", []):
+            filetype_id = filetype_str.removeprefix(".")
+            filetype_term = Term(
+                reference=default_reference(PREFIX, identifier=filetype_id, name=filetype_str),
+                type="Instance",
+            )
+            filetype_term.append_parent(ROOT_FILE_FORMAT)
+            filetype_term.annotate_string(has_source, MIMETYPE_IO_URL)
+            filetype_to_term[filetype_str] = filetype_term
+            yield filetype_term
 
     terms: dict[str, Term] = {}
     forwards: dict[Term, str] = {}
@@ -111,9 +124,11 @@ def get_terms() -> list[Term]:
                 term.append_see_also_uri(reference)
             terms[identifier.casefold()] = term
 
-            for filetype in mimetypes_record.get("fileTypes", []):
-                term.annotate_string(
-                    PREDICATE, filetype, annotations=[Annotation.uri(has_source, MIMETYPE_IO_URL)]
+            for filetype_str in mimetypes_record.get("fileTypes", []):
+                term.annotate_object(
+                    PREDICATE,
+                    filetype_to_term[filetype_str],
+                    annotations=[Annotation.uri(has_source, MIMETYPE_IO_URL)],
                 )
 
             if "in favor of" in name:
@@ -125,7 +140,7 @@ def get_terms() -> list[Term]:
             new = "application/vnd.afpc.afplinedata"
         old.append_replaced_by(terms[new].reference)
 
-    return list(terms.values())
+    yield from terms.values()
 
 
 def _process_references(cell: str) -> list[str]:
