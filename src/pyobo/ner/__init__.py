b88ca99527600e1fbe6306d7d95d83f6a99ed92a
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-09-13 00:36:22 +0200

    Update __init__.py

--- src/pyobo/ner/__init__.py
+++ src/pyobo/ner/__init__.py
@@ -2,8 +2,12 @@
 
 from .api import get_grounder
 from .normalizer import ground
+from .scispacy_utils import get_scispacy_entities, get_scispacy_entity_linker, get_scispacy_knowledgebase
 
 __all__ = [
     "get_grounder",
     "ground",
+    "get_scispacy_entities",
+    "get_scispacy_knowledgebase",
+    "get_scispacy_entity_linker",
 ]
