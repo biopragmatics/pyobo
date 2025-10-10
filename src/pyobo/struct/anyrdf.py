4f2ff80d7dae347d496291013a599a0e2ff19bb8
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-10-10 14:44:32 +0200

    assag

--- src/pyobo/struct/anyrdf.py
+++ src/pyobo/struct/anyrdf.py
@@ -1,32 +1,31 @@
-"""Read SKOS from RDF."""
+"""Read from RDF."""
 
 from pathlib import Path
 
 import curies
 import rdflib
-from bioregistry import NormalizedNamableReference, NormalizedNamedReference
-from rdflib import DCTERMS, RDF, SKOS, VANN, Graph, Node, URIRef, RDFS
+from bioregistry import NormalizedNamedReference
+from rdflib import OWL, RDF, RDFS, SKOS, Graph, Node, URIRef
 from tqdm import tqdm
 
 from pyobo.identifier_utils import get_converter
 from pyobo.struct import Obo, Term, build_ontology
 
 __all__ = [
-    "get_skos_ontology",
-    "read_skos",
+    "read_anyrdf",
 ]
 
 
-def read_skos(
+def read_anyrdf(
     path: str | Path, *, prefix: str | None = None, converter: curies.Converter | None = None
 ) -> Obo:
-    """Read a SKOS RDF file."""
+    """Read an RDF file."""
     graph = rdflib.Graph()
     graph.parse(path)
-    return get_skos_ontology(graph, prefix=prefix, converter=converter)
+    return _get_ontology(graph, prefix=prefix, converter=converter)
 
 
-def get_skos_ontology(
+def _get_ontology(
     graph: rdflib.Graph,
     *,
     prefix: str | None = None,
@@ -35,42 +34,18 @@ def get_skos_ontology(
     """Extract an ontology from a SKOS RDF graph."""
     if converter is None:
         converter = get_converter()
-    schemes = list(graph.subjects(RDF.type, SKOS.ConceptScheme))
-    if len(schemes) != 1:
-        raise ValueError
-    scheme = schemes[0]
-
-    def _get_scheme_object_literal(p: Node) -> str | None:
-        for o in graph.objects(scheme, p):
-            return str(o)
-        return None
-
-    if prefix is None:
-        prefix = _get_scheme_object_literal(VANN.preferredNamespacePrefix)
-
-    if prefix is None:
-        raise ValueError(f"no prefix given nor found using {VANN.preferredNamespacePrefix}")
-
-    root_terms = [
-        NormalizedNamableReference.from_reference(
-            converter.parse_uri(str(subject), strict=True).to_pydantic()
-        )
-        for subject in graph.objects(scheme, SKOS.hasTopConcept)
-    ]
     terms = [
-        get_term(graph, concept, converter=converter)
-        for concept in tqdm(graph.subjects(RDF.type, SKOS.Concept))
+        term
+        for concept in tqdm(
+            graph.subjects(RDF.type, [RDFS.Class, SKOS.Concept, OWL.Class, OWL.NamedIndividual])
+        )
+        if isinstance(concept, URIRef)
+           and (term := get_term(graph, concept, converter=converter)) is not None
     ]
-
-    # FIXME need to put in parents
-
     return build_ontology(
         prefix=prefix,
         terms=terms,
-        root_terms=root_terms,
         idspaces={curie_prefix: str(uri_prefix) for curie_prefix, uri_prefix in graph.namespaces()},
-        name=_get_scheme_object_literal(DCTERMS.title),
-        description=_get_scheme_object_literal(DCTERMS.description) or _get_scheme_object_literal(RDFS.comment),
     )
 
 
@@ -88,11 +63,15 @@ def _literal_objects(graph: Graph, subject: Node, predicate: Node) -> list[rdfli
 DEFAULT_LANGUAGES = {"en", None}
 
 
-def get_term(graph: rdflib.Graph, node: URIRef, converter: curies.Converter) -> Term:
+def get_term(graph: rdflib.Graph, node: URIRef, converter: curies.Converter) -> Term | None:
     """Get a term."""
-    reference_tuple = converter.parse_uri(str(node), strict=True)
-    labels = _literal_objects(graph, node, SKOS.prefLabel)
-    definitions = _literal_objects(graph, node, SKOS.definition)
+    reference_tuple = converter.parse_uri(str(node), strict=False)
+    if reference_tuple is None:
+        return None
+    labels = _literal_objects(graph, node, RDFS.label) or _literal_objects(
+        graph, node, SKOS.prefLabel
+    )
+    definitions = _literal_objects(graph, node, SKOS.definition)  # MULTIPLE
     term = Term(
         reference=NormalizedNamedReference(
             prefix=reference_tuple.prefix,
@@ -122,10 +101,10 @@ def get_term(graph: rdflib.Graph, node: URIRef, converter: curies.Converter) -> 
 def _demo():
     import pystow
 
-    url = "https://raw.githubusercontent.com/dini-ag-kim/hcrt/refs/heads/master/hcrt.ttl"
+    url = "https://nfdi4ing.pages.rwth-aachen.de/metadata4ing/metadata4ing/ontology.ttl"
     graph = pystow.ensure_rdf("dalia", url=url)
-    ontology = get_skos_ontology(graph)
-    ontology.write_obo("/Users/cthoyt/Desktop/hcrt.obo")
+    ontology = _get_ontology(graph, prefix="m4i")
+    ontology.write_obo("/Users/cthoyt/Desktop/m4i.obo")
 
 
 if __name__ == "__main__":
