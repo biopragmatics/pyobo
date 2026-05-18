31a0d55b34622084864606e79ec0131582f18836
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-05-18 14:17:22 +0200

    Update test_struct_term.py

--- tests/test_struct/test_obo/test_struct_term.py
+++ tests/test_struct/test_obo/test_struct_term.py
@@ -711,6 +711,26 @@ class TestTerm(unittest.TestCase):
             """,
         )
 
+    def test_12_property_string_with_quote(self) -> None:
+        """Test emitting a string property literal with a quote in it."""
+        term = Term(reference=LYSINE_DEHYDROGENASE_ACT)
+        term.annotate_string(RO_DUMMY, '"value" added')
+        self.assert_obo_stanza(
+            term,
+            obo="""\
+                [Term]
+                id: GO:0050069
+                name: lysine dehydrogenase activity
+                property_value: RO:1234567 "\\"value\\" added" xsd:string
+            """,
+            typedefs={RO_DUMMY.pair: RO_DUMMY},
+            ofn="""\
+                Declaration(Class(GO:0050069))
+                AnnotationAssertion(rdfs:label GO:0050069 "lysine dehydrogenase activity")
+                AnnotationAssertion(RO:1234567 GO:0050069 "\\"value\\" added"@en)
+            """,
+        )
+
     def test_12_property_integer(self) -> None:
         """Test emitting property literals that were annotated as a boolean."""
         term = Term(reference=LYSINE_DEHYDROGENASE_ACT)
