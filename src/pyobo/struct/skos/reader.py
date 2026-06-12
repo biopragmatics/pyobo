c70b414e0b43b9273f55307185aa0434e188a576
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-06-12 16:22:50 +0200

    Add object removal

--- src/pyobo/struct/skos/reader.py
+++ src/pyobo/struct/skos/reader.py
@@ -1,12 +1,15 @@
 """Read SKOS from RDF."""
 
 import itertools as itt
+from collections.abc import Iterable
 from pathlib import Path
 
 import curies
 import rdflib
 from bioregistry import NormalizedNamableReference, NormalizedNamedReference
 from bioregistry.schema import AnnotatedURL
+from curies import Reference
+from curies import vocabulary as v
 from rdflib import DCTERMS, RDF, RDFS, SKOS, VANN, Graph, Node, URIRef
 from tqdm import tqdm
 
@@ -76,30 +79,50 @@ def get_skos_from_rdflib(
         )
         for subject in graph.objects(scheme, SKOS.hasTopConcept)
     ]
-    terms = [
-        get_term(
+    terms: dict[Reference, Term] = {}
+    for concept in tqdm(
+        graph.subjects(RDF.type, SKOS.Concept),
+        desc=f"[{prefix}] SKOS concepts to OWL",
+        unit="term",
+        unit_scale=True,
+        leave=False,
+    ):
+        term = get_term(
             graph,
             concept,
             converter=converter,
             broad_match_becomes_parent=broad_match_becomes_parent,
         )
-        for concept in tqdm(
-            graph.subjects(RDF.type, SKOS.Concept),
-            desc=f"[{prefix}] SKOS concepts to OWL",
-            unit="term",
-            unit_scale=True,
-            leave=False,
-        )
-    ]
+        terms[term.reference] = term
+
+    _cleanup_narrow_matches(terms)
 
     if source is None:
         source = str(scheme)
 
+    # if SKOS-XL, like in tib.mbv, there might
+    # be multiple terms, one for each synonym.
+    # maybe map them as "alternate terms"? if there
+    # are multiple exact matches in the same ontology,
+    # and one but not the others has parents, then
+    # that one becomes the main term. otherwise, it's
+    # not clear how to do this
+
+    prefixes_used = {"dcterms", "rdfs", "iao"}
+    for term in terms.values():
+        prefixes_used.update(term._get_prefixes())
+
+    prefix_map = {
+        curie_prefix: str(uri_prefix)
+        for curie_prefix, uri_prefix in graph.namespaces()
+        if curie_prefix in prefixes_used
+    }
+
     return build_ontology(
         prefix=prefix,
-        terms=terms,
+        terms=list(terms.values()),
         root_terms=root_terms,
-        idspaces={curie_prefix: str(uri_prefix) for curie_prefix, uri_prefix in graph.namespaces()},
+        idspaces=prefix_map,
         name=_get_scheme_object_literal(DCTERMS.title),
         description=_get_scheme_object_literal(DCTERMS.description)
         or _get_scheme_object_literal(RDFS.comment),
@@ -107,18 +130,32 @@ def get_skos_from_rdflib(
     )
 
 
-def _literal_objects(graph: Graph, subject: Node, predicate: Node) -> list[rdflib.Literal]:
-    return [
-        o
-        for o in graph.objects(subject, predicate)
-        if isinstance(o, rdflib.Literal) and o._language in DEFAULT_LANGUAGES
-    ]
+def _cleanup_narrow_matches(terms: dict[Reference, Term]) -> None:
+    for term in terms.values():
+        for child in term.get_property_objects(v.narrow_match):
+            if isinstance(child, Reference) and child in terms and term in terms[child].parents:
+                term.remove_property_object(v.narrow_match, child)
+
+
+def _literal_objects(
+    graph: Graph, subject: Node, predicate: Node, language_priority: dict[str | None, int]
+) -> list[rdflib.Literal]:
+    language_literal_pairs: Iterable[tuple[str | None, rdflib.Literal]] = (
+        (literal._language, literal)
+        for literal in graph.objects(subject, predicate)
+        if isinstance(literal, rdflib.Literal) and literal._language in DEFAULT_LANGUAGES
+    )
+    langauge_literal_pairs = sorted(
+        language_literal_pairs, key=lambda lang_value: language_priority.get(lang_value[0], 1_000)
+    )
+    return [literal for _language, literal in langauge_literal_pairs]
 
 
 # until we have a better way of representing internationalization, this
 # just extracts a language-less or english language literal. otherwise,
 # it takes one at random
-DEFAULT_LANGUAGES = {"en", "en-US", None}
+DEFAULT_LANGUAGES = ["en", "en-US", None, "de"]
+DEFAULT_LANGUAGE_PRIORITY = {lang: i for i, lang in enumerate(DEFAULT_LANGUAGES)}
 
 
 def get_term(
@@ -128,9 +165,11 @@ def get_term(
     broad_match_becomes_parent: bool = True,
 ) -> Term:
     """Get a term."""
+    language_priority = DEFAULT_LANGUAGE_PRIORITY
+
     reference_tuple = converter.parse_uri(str(node), strict=True)
-    labels = _literal_objects(graph, node, SKOS.prefLabel)
-    definitions = _literal_objects(graph, node, SKOS.definition)
+    labels = _literal_objects(graph, node, SKOS.prefLabel, language_priority)
+    definitions = _literal_objects(graph, node, SKOS.definition, language_priority)
     term = Term(
         reference=NormalizedNamedReference(
             prefix=reference_tuple.prefix,
@@ -139,8 +178,9 @@ def get_term(
         ),
         definition=definitions[0] if definitions else None,
     )
-    for alt in _literal_objects(graph, node, SKOS.altLabel):
-        term.append_synonym(alt)
+    for alt in _literal_objects(graph, node, SKOS.altLabel, language_priority):
+        if alt._language in language_priority:
+            term.append_synonym(alt, language=alt._language)
 
     for exact_match in graph.objects(node, SKOS.exactMatch):
         if isinstance(exact_match, URIRef):
@@ -178,7 +218,14 @@ def _demo() -> None:
     from tabulate import tabulate
 
     rows = []
-    for resource in bioregistry.resources():
+
+    # for resource in bioregistry.resources():
+    #     rdf = resource.get_download_rdf(get_format=True)
+    #     if rdf and not resource.get_download_skos():
+    #         print(resource.prefix, rdf)
+    # return
+
+    for resource in tqdm(bioregistry.resources()):
         match resource.get_download_skos(get_format=True):
             case None:
                 continue
@@ -188,15 +235,22 @@ def _demo() -> None:
                 except SyntaxError:
                     tqdm.write(f"need explicit RDF format for {resource.prefix}")
                     continue
-                rows.append((resource.prefix, url, "", len(list(ontology.iter_terms()))))
+                rows.append((resource.prefix, url, "", *_summarize(ontology)))
             case AnnotatedURL() as model:
                 ontology = read_skos(model.url, prefix=resource.prefix, rdf_format=model.rdf_format)
-                rows.append(
-                    (resource.prefix, model.url, model.rdf_format, len(list(ontology.iter_terms())))
-                )
+                rows.append((resource.prefix, model.url, model.rdf_format, *_summarize(ontology)))
         ontology.write_obo(f"/Users/cthoyt/Desktop/{resource.prefix}.obo")
 
-    tqdm.write(tabulate(rows))
+    tqdm.write(tabulate(rows, headers=["prefix", "url", "format", "terms", "parents"]))
+
+
+def _summarize(ontology: Obo) -> tuple[int, ...]:
+    n_parents = 0
+    n_terms = 0
+    for term in ontology:
+        n_terms += 1
+        n_parents += len(term.parents)
+    return n_terms, n_parents
 
 
 if __name__ == "__main__":
