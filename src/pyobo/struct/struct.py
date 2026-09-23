3090771c502f68192e1d651868b7231bdb921a86
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-09-23 12:01:45 +0200

    Update struct.py

--- src/pyobo/struct/struct.py
+++ src/pyobo/struct/struct.py
@@ -16,7 +16,7 @@ from collections.abc import Callable, Collection, Iterable, Iterator, Mapping, S
 from dataclasses import dataclass, field
 from pathlib import Path
 from textwrap import dedent
-from typing import Annotated, Any, ClassVar, Literal, Self, TextIO, cast, overload
+from typing import IO, Annotated, Any, ClassVar, Literal, Self, cast, overload
 
 import bioregistry
 import click
@@ -1148,7 +1148,7 @@ class Obo:
 
     def write_obo(
         self,
-        file: str | TextIO | Path | None = None,
+        file: str | IO[str] | Path | None = None,
         *,
         progress: bool = False,
         emit_object_properties: bool = True,
@@ -1174,7 +1174,7 @@ class Obo:
             self._write_lines(it, file)
 
     @staticmethod
-    def _write_lines(it: Iterable[str], file: TextIO | None) -> None:
+    def _write_lines(it: Iterable[str], file: IO[str] | None) -> None:
         for line in it:
             print(line, file=file)
 
